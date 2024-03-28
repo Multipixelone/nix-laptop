832c60422b7986ad66bd6768ac8341c4f73f7dc9
Multipixelone <finn@cnwr.net>
2024-03-28 00:13:17 -0400

    split out binds file

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -18,6 +18,9 @@
     sha256 = "sha256:14nh77xn8x58693y2na5askm6612xqbll2kr6237y8pjr1jc24xp";
   };
 in {
+  imports = [
+    ./binds.nix
+  ];
   services.mako = {
     enable = true;
     borderColor = pkgs.lib.mkForce "#cba6f7";
@@ -58,7 +61,6 @@ in {
       gaps_out = 5;
       "col.inactive_border" = pkgs.lib.mkForce "rgb(1e1e2e)";
       "col.active_border" = pkgs.lib.mkForce "rgb(cba6f7)";
-
     };
     misc = {
       disable_hyprland_logo = true;
@@ -70,48 +72,6 @@ in {
         natural_scroll = true;
       };
     };
-    "$mod" = "ALT";
-    bind = [
-      "ALT_SHIFT, Q, killactive"
-      "$mod, RETURN, exec, ${terminal}"
-      "ALT_SHIFT, W, exec, firefox"
-      "$mod, SPACE, exec, ${launcher}"
-      "$mod, M, exit"
-      "$mod, V, togglefloating"
-      "SUPER, F, fullscreen"
-      "SUPER, L, exec, ${lock}"
-      "$mod, 1, workspace, 1"
-      "$mod, 2, workspace, 2"
-      "$mod, 3, workspace, 3"
-      "$mod, 4, workspace, 4"
-      "$mod, 5, workspace, 5"
-      "$mod, 6, workspace, 6"
-      "$mod, 7, workspace, 7"
-      "$mod, 8, workspace, 8"
-      "$mod, 9, workspace, 9"
-      "$mod SHIFT, 1, movetoworkspace, 1"
-      "$mod SHIFT, 2, movetoworkspace, 2"
-      "$mod SHIFT, 3, movetoworkspace, 3"
-      "$mod SHIFT, 4, movetoworkspace, 4"
-      "$mod SHIFT, 5, movetoworkspace, 5"
-      "$mod SHIFT, 6, movetoworkspace, 6"
-      "$mod SHIFT, 7, movetoworkspace, 7"
-      "$mod SHIFT, 8, movetoworkspace, 8"
-      "$mod SHIFT, 9, movetoworkspace, 9"
-    ];
-    bindm = [
-      # mouse movements
-      "$mod, mouse:272, movewindow"
-      "$mod, mouse:273, resizewindow"
-      #"$mod ALT, mouse:272, resizewindow"
-    ];
-    binde = [
-      ", XF86AudioRaiseVolume, exec, ${swayosd-client} --output-volume raise"
-      ", XF86AudioLowerVolume, exec, ${swayosd-client} --output-volume lower"
-      ", XF86AudioMute, exec, ${swayosd-client} --output-volume mute-toggle"
-      ", XF86MonBrightnessUp, exec, ${swayosd-client} --brightness raise"
-      ", XF86MonBrightnessDown, exec, ${swayosd-client} --brightness lower"
-    ];
     xwayland = {
       force_zero_scaling = true;
     };
