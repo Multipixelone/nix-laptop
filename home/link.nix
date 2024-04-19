38037f2701458a41b2a1a70a0e16e62697abdb26
Multipixelone <finn@cnwr.net>
2024-04-19 00:54:33 -0400

    start ledfx script

--- home/link.nix
+++ home/link.nix
@@ -10,4 +10,13 @@
   imports = [
     ./core.nix
   ];
+  systemd.user.services.ledfx = {
+    Unit.Description = "ledfx light control";
+    Install.WantedBy = ["graphical-session.target"];
+    Service = {
+      ExecStart = "${pkgs.ledfx}/bin/ledfx";
+      ExecStartPost = ''${pkgs.curl}/bin/curl -X 'PUT' 'http://link.bun-hexatonic.ts.net:8888/api/scenes' -H 'Content-Type: application/json' -d '{"id": "main-purple", "action": "activate"}'
+      '';
+    };
+  };
 }
