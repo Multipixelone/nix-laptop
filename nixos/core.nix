12f7a3be45f2ef1574e68fcfc15b29466cc02f33
Multipixelone <finn@cnwr.net>
2024-04-28 23:40:55 -0400

    re-add qGIS

--- nixos/core.nix
+++ nixos/core.nix
@@ -43,9 +43,9 @@
     })
     yabridgectl
     pulseaudioFull
-    #(inputs.geospatial.packages.${pkgs.system}.qgis.override {
-    #  extraPythonPackages = ps: [ps.pandas ps.numpy ps.scipy ps.pandas ps.charset-normalizer ps.click-plugins ps.click ps.certifi ps.cligj ps.colorama ps.fiona ps.pyproj ps.pytz ps.requests ps.rtree ps.setuptools ps.shapely ps.six ps.tzdata ps.zipp ps.attrs ps.dateutil ps.python-dateutil ps.idna ps.importlib-metadata ps.pyaml ps.urllib3 ps.packaging ps.cython ps.ortools ps.numexpr ps.py-cpuinfo ps.tables ps.fastparquet (callPackage ../pkgs/aequilibrae/default.nix {})];
-    #})
+    (inputs.geospatial.packages.${pkgs.system}.qgis.override {
+      extraPythonPackages = ps: [ps.pandas ps.numpy ps.scipy ps.pandas ps.charset-normalizer ps.click-plugins ps.click ps.certifi ps.cligj ps.colorama ps.fiona ps.pyproj ps.pytz ps.requests ps.rtree ps.setuptools ps.shapely ps.six ps.tzdata ps.zipp ps.attrs ps.dateutil ps.python-dateutil ps.idna ps.importlib-metadata ps.pyaml ps.urllib3 ps.packaging ps.cython ps.ortools ps.numexpr ps.py-cpuinfo ps.tables ps.fastparquet (callPackage ../pkgs/aequilibrae/default.nix {})];
+    })
     libspatialite
   ];
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
