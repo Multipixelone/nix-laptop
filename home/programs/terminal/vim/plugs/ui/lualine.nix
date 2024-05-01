909e235a74f78a5d44f7b8a4e87d485524046fdc
Multipixelone <finn@cnwr.net>
2024-05-01 00:24:16 -0400

    fix lualine

--- home/programs/terminal/vim/plugs/ui/lualine.nix
+++ home/programs/terminal/vim/plugs/ui/lualine.nix
@@ -1,125 +1,33 @@
 {
   plugins.lualine = {
-    enable = true;
+    enable = false;
+    alwaysDivideMiddle = true;
     globalstatus = true;
-    disabledFiletypes = {
-      statusline = ["dashboard" "alpha"];
-    };
-    theme = {
-      normal = {
-        a = {
-          bg = "#b4befe";
-          fg = "#1c1d21";
-        };
-        b = {
-          bg = "nil";
-        };
-        c = {
-          bg = "nil";
-        };
-        z = {
-          bg = "nil";
-        };
-        y = {
-          bg = "nil";
-        };
-        x = {
-          bg = "nil";
-        };
-      };
-    };
+    ignoreFocus = ["neo-tree"];
+    extensions = ["fzf"];
+    theme = "auto";
     componentSeparators = {
-      left = "";
-      right = " ";
+      left = "|";
+      right = "|";
     };
     sectionSeparators = {
-      left = "";
-      right = "";
+      left = "█"; # 
+      right = "█"; # 
     };
     sections = {
-      lualine_a = [
-        {
-          name = "mode";
-          icon = "";
-          separator = {
-            left = "";
-            right = "";
-          };
-        }
-      ];
+      lualine_a = ["mode"];
       lualine_b = [
         {
           name = "branch";
           icon = "";
-          separator = {
-            left = "";
-            right = "";
-          };
-          color = {
-            fg = "#1c1d21";
-            bg = "#7d83ac";
-          };
         }
-      ];
-      lualine_c = [
-        {
-          name = "diagnostic";
-          extraConfig = {
-            symbols = {
-              error = " ";
-              warn = " ";
-              info = " ";
-              hint = "󰝶 ";
-            };
-          };
-        }
-        {
-          name = "filetype";
-          separator = {
-            left = "";
-            right = "";
-          };
-          extraConfig = {
-            icon_only = true;
-            padding = {
-              left = 1;
-              right = 0;
-            };
-          };
-        }
-        {
-          name = "filename";
-          extraConfig = {
-            symbols = {
-              modified = "  ";
-              readonly = "";
-              unnamed = "";
-            };
-          };
-        }
-      ];
-      lualine_x = [
         "diff"
+        "diagnostics"
       ];
-      lualine_y = [
-        {
-          name = "progress";
-          icon = "";
-          color = {
-            fg = "#1c1d21";
-            bg = "#f2cdcd";
-          };
-        }
-      ];
-      lualine_z = [
-        {
-          name = "location";
-          color = {
-            fg = "#1c1d21";
-            bg = "#f2cdcd";
-          };
-        }
-      ];
+      lualine_c = ["filename"];
+      lualine_x = ["filetype"];
+      lualine_y = ["progress"];
+      lualine_z = [''" " .. os.date("%R")''];
     };
   };
 }
