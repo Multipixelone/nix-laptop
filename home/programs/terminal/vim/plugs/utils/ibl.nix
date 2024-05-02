00b0342ba640318712da02eb3fc72f3781e23d7e
Multipixelone <finn@cnwr.net>
2024-05-02 03:52:58 -0400

    fix indent blankline config

--- home/programs/terminal/vim/plugs/utils/ibl.nix
+++ home/programs/terminal/vim/plugs/utils/ibl.nix
@@ -1,5 +1,28 @@
 {
   plugins.indent-blankline = {
     enable = true;
+    settings = {
+      indent = {
+        char = "│";
+      };
+      scope = {
+        show_start = false;
+        show_end = false;
+        show_exact_scope = true;
+      };
+      exclude = {
+        filetypes = [
+          ""
+          "checkhealth"
+          "help"
+          "lspinfo"
+          "packer"
+          "TelescopePrompt"
+          "TelescopeResults"
+          "yaml"
+        ];
+        buftypes = ["terminal" "quickfix"];
+      };
+    };
   };
 }
