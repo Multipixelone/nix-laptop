7f9d833e7b95249b7b32cce7baccb94428835ce6
Multipixelone <finn@cnwr.net>
2024-05-02 04:35:54 -0400

    noice presets

--- home/programs/terminal/vim/plugs/ui/noice.nix
+++ home/programs/terminal/vim/plugs/ui/noice.nix
@@ -16,6 +16,13 @@
         view = "mini";
       };
     };
+    presets = {
+      bottom_search = false;
+      command_palette = true;
+      long_message_to_split = false;
+      lsp_doc_border = true;
+      inc_rename = true;
+    };
     popupmenu = {
       enabled = true;
       backend = "nui";
