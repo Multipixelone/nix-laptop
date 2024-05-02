4ec9b34d422f4c8990b18b36b96249a212bc84a4
Multipixelone <finn@cnwr.net>
2024-05-02 03:20:22 -0400

    add ctrl+s

--- home/programs/terminal/vim/keys.nix
+++ home/programs/terminal/vim/keys.nix
@@ -1,3 +1,13 @@
 {
   globals.mapleader = " ";
+  keymaps = [
+    {
+      mode = "n";
+      key = "<c-s>";
+      action = "<cmd>w<CR>";
+      options = {
+        desc = "Save File";
+      };
+    }
+  ];
 }
