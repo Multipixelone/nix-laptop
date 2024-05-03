d3e7297e24ceeea2daa86050454bd43e969f7461
Multipixelone <finn@cnwr.net>
2024-05-03 11:35:35 -0400

    don't need filesystem definition for rpi

--- nixos/rpidns1.nix
+++ nixos/rpidns1.nix
@@ -19,11 +19,6 @@
     #nameservers = ["192.168.6.111" "192.168.6.112"];
     nameservers = ["8.8.8.8" "4.4.4.4"];
   };
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/576fdcd4-d642-4229-9073-90724eb72043";
-    fsType = "btrfs";
-    options = ["subvol=@" "compress=zstd:4" "ssd" "relatime" "discard=async"];
-  };
   swapDevices = [];
   networking.useDHCP = lib.mkDefault false;
 }
