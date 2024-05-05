7c4c55eb172ce3299557bbff89ca7e8af48a54f7
Multipixelone <finn@cnwr.net>
2024-05-05 16:21:03 -0400

    add qt theme

--- home/programs/theming/default.nix
+++ home/programs/theming/default.nix
@@ -18,7 +18,23 @@
     };
     #font = (pkgs.callPackage ../pkgs/pragmata/default.nix {});
   };
+  qt = {
+    enable = true;
+    platformTheme.name = "qtct";
+    style = {
+      name = "Catppuccin-Frappe-Dark";
+      package = pkgs.catppuccin-kde.override {
+        flavour = ["mocha"];
+        accents = ["mauve"];
+      };
+    };
+  };
   home.packages = with pkgs; [
+    qt5.qttools
+    qt6Packages.qtstyleplugin-kvantum
+    libsForQt5.qtstyleplugin-kvantum
+    libsForQt5.qt5ct
+    breeze-icons
     (nerdfonts.override {fonts = ["FiraCode"];})
     (pkgs.callPackage ../../../pkgs/pragmata/default.nix {})
   ];
