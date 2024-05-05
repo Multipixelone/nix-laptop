70e59e9ba9bb4736e02bf4fbd2b33ea0fbd5fc25
Multipixelone <finn@cnwr.net>
2024-05-05 16:22:47 -0400

    add some gtk extra config

--- home/programs/theming/default.nix
+++ home/programs/theming/default.nix
@@ -16,6 +16,20 @@
       package = pkgs.catppuccin-papirus-folders;
       name = "Papirus-Dark";
     };
+    gtk3.extraConfig = {
+      gtk-xft-antialias = 1;
+      gtk-xft-hinting = 1;
+      gtk-xft-hintstyle = "hintslight";
+      gtk-xft-rgba = "rgb";
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+    gtk2.extraConfig = ''
+      gtk-xft-antialias=1
+      gtk-xft-hinting=1
+      gtk-xft-hintstyle="hintslight"
+      gtk-xft-rgba="rgb"
+    '';
     #font = (pkgs.callPackage ../pkgs/pragmata/default.nix {});
   };
   qt = {
