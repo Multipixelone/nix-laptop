bf358870c32ea67fdfd1b84d771fedaa3152e7dc
Multipixelone <finn@cnwr.net>
2024-05-13 23:28:24 -0400

    start packaging bluebubbles

---
+++ pkgs/bluebubbles/default.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  flutter,
+  makeDesktopItem,
+  fetchFromGitHub,
+}: let
+  pubspecLock = ./pubspec.lock.json;
+in
+  flutter.buildFlutterApplication rec {
+    pname = "BlueBubbles";
+    version = "1.13.2";
+
+    src = fetchFromGitHub {
+      owner = "BlueBubblesApp";
+      repo = "bluebubbles-app";
+      rev = "refs/tags/v${version}";
+      sha256 = "sha256-eF1xzZTmLxgN/qKxAFtMy0Lsl7WrZzBxgHSIjfy1pJk=";
+    };
+    inherit pubspecLock;
+  }
