aeb48156dcf6684fcec4bffd25b5dff878afe8de
Multipixelone <finn@cnwr.net>
2024-05-19 14:07:06 -0400

    add libreoffice

--- home/programs/latex/default.nix
+++ home/programs/latex/default.nix
@@ -3,5 +3,8 @@
     zotero
     texliveFull
     latexrun
+    # TODO Maybe move Libreoffice to some other kind of general editing module? This just seemed like the most sensible place rn
+    libreoffice-fresh
+    jdk # Needed for libreoffice?
   ];
 }
