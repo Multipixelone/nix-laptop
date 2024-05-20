f44ccafacc7fbb65938fa1fbe8fd0ade6ecc9f09
Multipixelone <finn@cnwr.net>
2024-05-20 19:00:06 -0400

    additional mangohud config

--- home/programs/gaming/default.nix
+++ home/programs/gaming/default.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   home.packages = with pkgs; [
     gamescope
     discord
@@ -10,5 +14,22 @@
   programs.mangohud = {
     enable = true;
     enableSessionWide = true;
+    settings = {
+      # fix some weird stylix defaults
+      background_alpha = lib.mkForce 0.0;
+      font_size = lib.mkForce 24;
+      font_scale = lib.mkForce 1.0;
+      # custom stuff
+      text_outline_thickness = 2;
+      # horizontal = true; # TODO write a mangohud patch to remove the forced newline before the music module
+      wine = true;
+      time = true;
+      time_no_label = true;
+      frametime = false;
+      histogram = false;
+      fps_value = "90,144,240";
+      media_player = true;
+      media_player_format = "{artist} - {title}";
+    };
   };
 }
