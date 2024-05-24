684b59b45c8dada5398eb11494569e4ea4febe23
Multipixelone <finn@cnwr.net>
2024-05-24 12:12:54 -0400

    get config settings from steam deck

--- home/programs/gaming/mangohud.nix
+++ home/programs/gaming/mangohud.nix
@@ -22,16 +22,23 @@
     };
     settings = {
       # fix some weird stylix defaults
-      background_alpha = lib.mkForce 0.0;
+      background_alpha = lib.mkForce 0.4;
       font_size = lib.mkForce 24;
+      font_size_text = lib.mkForce 24;
+      no_small_font = true;
       font_scale = lib.mkForce 1.0;
+      font_scale_media_player = 1.0;
       # custom stuff
+      hud_no_margin = true;
       text_outline_thickness = 2;
       horizontal = true;
       wine = true;
       time = true;
-      resolution = true;
+      vram = true;
+      ram = true;
+
       time_no_label = true;
+      time_format = "%H:%M";
       frame_timing = 0;
       fps_value = "90,144,240";
       media_player = true;
