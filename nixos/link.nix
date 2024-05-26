d1740fb5ca918a0f7106da4b970621ba4dbeb480
Multipixelone <finn@cnwr.net>
2024-05-26 14:29:59 -0400

    fix font in kmscon

--- nixos/link.nix
+++ nixos/link.nix
@@ -3,7 +3,6 @@
   pkgs,
   lib,
   inputs,
-  stylix,
   ...
 }: {
   imports = [
@@ -21,20 +20,15 @@
     keyMap = "us";
   };
   services.kmscon = {
-    # Use kmscon as the virtual console instead of gettys.
-    # kmscon is a kms/dri-based userspace virtual terminal implementation.
-    # It supports a richer feature set than the standard linux console VT,
-    # including full unicode support, and when the video card supports drm should be much faster.
     enable = true;
     fonts = [
       {
-        name = "Source Code Pro";
-        package = pkgs.source-code-pro;
+        name = "PragmataPro Mono Liga";
+        package = pkgs.callPackage ../pkgs/pragmata/default.nix {};
       }
     ];
     extraOptions = "--term xterm-256color";
     extraConfig = "font-size=12";
-    # Whether to use 3D hardware acceleration to render the console.
     hwRender = true;
   };
   networking.useDHCP = lib.mkDefault false;
