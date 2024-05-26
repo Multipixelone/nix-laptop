70a1af7aa88de33c58aee101da8813cdb9816469
Multipixelone <finn@cnwr.net>
2024-05-26 14:47:51 -0400

    disable openrgb for now, as it causes stutters

--- nixos/modules/rgb.nix
+++ nixos/modules/rgb.nix
@@ -1,17 +1,12 @@
-{
-  inputs,
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  environment.systemPackages = with pkgs; [
-    openrgb
-  ];
+{...}: {
+  # TODO fix openrgb, or figure out why it causes microstutters w audio
+  # environment.systemPackages = with pkgs; [
+  #   openrgb
+  # ];
   programs.coolercontrol.enable = true;
-  services.udev.packages = [pkgs.openrgb];
+  # services.udev.packages = [pkgs.openrgb];
   boot.kernelParams = ["acpi_enforce_resources=lax"];
   boot.kernelModules = ["i2c-dev" "i2c-piix4"];
-  hardware.openrazer.enable = true;
-  services.hardware.openrgb.enable = true;
+  # hardware.openrazer.enable = true;
+  # services.hardware.openrgb.enable = true;
 }
