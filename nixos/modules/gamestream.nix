a0f3ac94447e6a85280564138c28929ee5de7aa3
Multipixelone <finn@cnwr.net>
2024-05-27 19:06:55 -0400

    don't relock, causing weird issues

--- nixos/modules/gamestream.nix
+++ nixos/modules/gamestream.nix
@@ -28,15 +28,15 @@
   undo-command =
     pkgs.writeShellApplication {
       name = "undo-command";
-      runtimeInputs = [pkgs.findutils pkgs.gawk pkgs.coreutils pkgs.curl pkgs.hyprland pkgs.hyprlock];
+      runtimeInputs = [pkgs.findutils pkgs.gawk pkgs.coreutils pkgs.curl pkgs.hyprland];
 
       text = ''
         mon_string="DP-1,2560x1440@240,1200x0,1"
         #curl -X 'PUT' 'http://link.bun-hexatonic.ts.net:8888/api/scenes' -H 'Content-Type: application/json' -d '{"id": "main-purple", "action": "activate"}'
         hyprctl keyword monitor "DP-3,1920x1200@60,0x0,1,transform,1"
         hyprctl keyword monitor "$mon_string"
-        # TODO Eventually figure out some non jank way to not display the broken desktop lol
-        hyprlock
+        # # TODO Eventually figure out some non jank way to not display the broken desktop lol. re-locking disabled for now
+        # hyprlock
       '';
     }
     + "/bin/undo-command";
