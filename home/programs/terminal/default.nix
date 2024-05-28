9174cfe4c02752ffcef7a646822780e19bc61051
Multipixelone <finn@cnwr.net>
2024-05-28 19:05:52 -0400

    custom fzf default command

--- home/programs/terminal/default.nix
+++ home/programs/terminal/default.nix
@@ -47,6 +47,9 @@
     fzf = {
       enable = true;
       enableFishIntegration = true;
+      defaultCommand = "fd --type file --follow"; # FZF_DEFAULT_COMMAND
+      defaultOptions = ["--height 20%"]; # FZF_DEFAULT_OPTS
+      fileWidgetCommand = "fd --type file --follow"; # FZF_CTRL_T_COMMAND
     };
   };
 }
