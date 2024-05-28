ead75dbf1997852adc6c8acbf08faaa0c5d079a0
Multipixelone <finn@cnwr.net>
2024-05-28 18:57:53 -0400

    git sane default config

--- home/server.nix
+++ home/server.nix
@@ -28,8 +28,29 @@
   programs = {
     git = {
       enable = true;
+      lfs.enable = true;
+      delta.enable = true;
+
       userName = "Multipixelone";
       userEmail = "finn@cnwr.net";
+
+      extraConfig = {
+        init.defaultBranch = "main";
+        branch.autosetupmerge = "true";
+        push.default = "current";
+        merge.stat = "true";
+        core.whitespace = "fix,-indent-with-non-tab,trailing-space,cr-at-eol";
+        repack.usedeltabaseoffset = "true";
+        pull.ff = "only";
+        rebase = {
+          autoSquash = true;
+          autoStash = true;
+        };
+        rerere = {
+          enabled = true;
+          autoupdate = true;
+        };
+      };
     };
     ssh = {
       enable = true;
