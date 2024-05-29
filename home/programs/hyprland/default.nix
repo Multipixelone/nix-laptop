6b431c6242921a7e86652279b0f027de071c3610
Multipixelone <finn@cnwr.net>
2024-05-29 00:53:58 -0400

    merge all wayland.windowManager.hyprland

--- home/programs/hyprland/default.nix
+++ home/programs/hyprland/default.nix
@@ -45,97 +45,104 @@ in {
       '';
     })
   ];
-  wayland.windowManager.hyprland.enable = true;
-  wayland.windowManager.hyprland.plugins = [
-    #inputs.Hyprspace.packages.${pkgs.system}.Hyprspace
-    #inputs.hyprgrass.packages.${pkgs.system}.default
-  ];
-  wayland.windowManager.hyprland.systemd.variables = ["--all"];
-  wayland.windowManager.hyprland.extraConfig = ''
-    env = XDG_SCREENSHOTS_DIR,/home/tunnel/Pictures/Screenshots
-    env = QT_QPA_PLATFORM,wayland
-    env = QT_QPA_PLATFORMTHEME,qt5ct
-    env = XCURSOR_SIZE,32
-    env = XDG_CURRENT_DESKTOP,Hyprland
-    env = XDG_SESSION_TYPE,wayland
-    env = XDG_SESSION_DESKTOP,Hyprland
-    env = MOZ_ENABLE_WAYLAND,1
-    # TODO figure out why INSTANCE_SIGNATURE not working.
-    exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP HYPRLAND_INSTANCE_SIGNATURE
-    bezier = wind, 0.05, 0.9, 0.1, 1.05
-    bezier = winIn, 0.1, 1.1, 0.1, 1.1
-    bezier = winOut, 0.3, -0.3, 0, 1
-    bezier = liner, 1, 1, 1, 1
-    bezier = linear, 0.0, 0.0, 1.0, 1.0
-
-    animations {
-        enabled = true
-        # bezier = overshot, 0.05, 0.9, 0.1, 1.1
-        bezier = overshot, 0.13, 0.99, 0.29, 1.1
-        animation = windows, 1, 4, overshot, slide
-        animation = border, 1, 10, default
-        animation = fade, 1, 10, default
-        animation = workspaces, 1, 6, overshot, slide
-    }
-  '';
-  wayland.windowManager.hyprland.settings = {
-    # FIX Kinda jank mkMerge
-    monitorSettings = lib.mkMerge [
-      (lib.mkIf (osConfig.networking.hostName == "link") {monitor = ["DP-1,2560x1440@240,1200x0,1" "DP-3,1920x1200@60,0x0,1,transform,1"];})
-      (lib.mkIf (osConfig.networking.hostName == "zelda") {monitor = [",highres,auto,2"];})
+  # TODO reorganize all of this and make it cleaner
+  # TODO move all env def into session vars
+  wayland.windowManager.hyprland = {
+    enable = true;
+    systemd = {
+      enable = true;
+      variables = ["--all"];
+    };
+    plugins = [
+      #inputs.Hyprspace.packages.${pkgs.system}.Hyprspace
+      #inputs.hyprgrass.packages.${pkgs.system}.default
     ];
-    exec-once = [polkit agent waybar notifs swayosd-server music-term pypr];
-    decoration = {
-      rounding = "6";
-      shadow_offset = "0 2";
-      drop_shadow = true;
-      shadow_ignore_window = true;
-      shadow_range = 20;
-      shadow_render_power = 3;
-      "col.shadow" = pkgs.lib.mkForce "rgba(00000055)";
-      blur = {
-        enabled = true;
-        brightness = 1.1;
-        noise = 0.0117;
-        passes = 3;
-        size = 16;
-        ignore_opacity = true;
+    extraConfig = ''
+      env = XDG_SCREENSHOTS_DIR,/home/tunnel/Pictures/Screenshots
+      env = QT_QPA_PLATFORM,wayland
+      env = QT_QPA_PLATFORMTHEME,qt5ct
+      env = XCURSOR_SIZE,32
+      env = XDG_CURRENT_DESKTOP,Hyprland
+      env = XDG_SESSION_TYPE,wayland
+      env = XDG_SESSION_DESKTOP,Hyprland
+      env = MOZ_ENABLE_WAYLAND,1
+      # TODO figure out why INSTANCE_SIGNATURE not working.
+      exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP HYPRLAND_INSTANCE_SIGNATURE
+      bezier = wind, 0.05, 0.9, 0.1, 1.05
+      bezier = winIn, 0.1, 1.1, 0.1, 1.1
+      bezier = winOut, 0.3, -0.3, 0, 1
+      bezier = liner, 1, 1, 1, 1
+      bezier = linear, 0.0, 0.0, 1.0, 1.0
+
+      animations {
+          enabled = true
+          # bezier = overshot, 0.05, 0.9, 0.1, 1.1
+          bezier = overshot, 0.13, 0.99, 0.29, 1.1
+          animation = windows, 1, 4, overshot, slide
+          animation = border, 1, 10, default
+          animation = fade, 1, 10, default
+          animation = workspaces, 1, 6, overshot, slide
+      }
+    '';
+    settings = {
+      # FIX Kinda jank mkMerge
+      monitorSettings = lib.mkMerge [
+        (lib.mkIf (osConfig.networking.hostName == "link") {monitor = ["DP-1,2560x1440@240,1200x0,1" "DP-3,1920x1200@60,0x0,1,transform,1"];})
+        (lib.mkIf (osConfig.networking.hostName == "zelda") {monitor = [",highres,auto,2"];})
+      ];
+      exec-once = [polkit agent waybar notifs swayosd-server music-term pypr];
+      decoration = {
+        rounding = "6";
+        shadow_offset = "0 2";
+        drop_shadow = true;
+        shadow_ignore_window = true;
+        shadow_range = 20;
+        shadow_render_power = 3;
+        "col.shadow" = pkgs.lib.mkForce "rgba(00000055)";
+        blur = {
+          enabled = true;
+          brightness = 1.1;
+          noise = 0.0117;
+          passes = 3;
+          size = 16;
+          ignore_opacity = true;
+        };
+        #"col.shadow" = "rgba(00000099)";
       };
-      #"col.shadow" = "rgba(00000099)";
-    };
-    general = {
-      border_size = 3;
-      gaps_in = 5;
-      gaps_out = 5;
-      "col.inactive_border" = pkgs.lib.mkForce "rgb(1e1e2e)";
-      "col.active_border" = pkgs.lib.mkForce "rgb(cba6f7)";
-    };
-    dwindle = {
-      # keep floating dimentions while tiling
-      pseudotile = true;
-      preserve_split = true;
-    };
-    gestures = {
-      workspace_swipe = true;
-      workspace_swipe_forever = true;
-      workspace_swipe_cancel_ratio = 0.15;
-    };
-    misc = {
-      disable_autoreload = true;
-      force_default_wallpaper = 0;
-      disable_hyprland_logo = true;
-      disable_splash_rendering = true;
-      vfr = true;
-    };
-    input = {
-      accel_profile = "flat";
-      touchpad = {
-        natural_scroll = true;
-        disable_while_typing = false;
+      general = {
+        border_size = 3;
+        gaps_in = 5;
+        gaps_out = 5;
+        "col.inactive_border" = pkgs.lib.mkForce "rgb(1e1e2e)";
+        "col.active_border" = pkgs.lib.mkForce "rgb(cba6f7)";
       };
-    };
-    xwayland = {
-      force_zero_scaling = true;
+      dwindle = {
+        # keep floating dimentions while tiling
+        pseudotile = true;
+        preserve_split = true;
+      };
+      gestures = {
+        workspace_swipe = true;
+        workspace_swipe_forever = true;
+        workspace_swipe_cancel_ratio = 0.15;
+      };
+      misc = {
+        disable_autoreload = true;
+        force_default_wallpaper = 0;
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+        vfr = true;
+      };
+      input = {
+        accel_profile = "flat";
+        touchpad = {
+          natural_scroll = true;
+          disable_while_typing = false;
+        };
+      };
+      xwayland = {
+        force_zero_scaling = true;
+      };
     };
   };
   home.file = {
