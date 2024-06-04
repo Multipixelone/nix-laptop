9535b2dae8decac6d9a811c530da0aacf16f0c00
Multipixelone <finn@cnwr.net>
2024-06-04 00:00:49 -0400

    enable wine fync

--- home/desktop.nix
+++ home/desktop.nix
@@ -12,6 +12,7 @@
     RCLONE_CONFIG = config.age.secrets."restic/rclone".path;
     RESTIC_REPOSITORY = "rclone:onedrive:Backups/${osConfig.networking.hostName}";
     MOPIDY_PLAYLISTS = "/home/tunnel/.local/share/mopidy/m3u";
+    WINEFSYNC = 1;
   };
   imports = [
     ./server.nix
