b68d00897507dfe60e50d546678ec501c59f97a0
Multipixelone <finn@cnwr.net>
2024-06-14 12:51:14 -0400

    add jdownloader container

--- nixos/link.nix
+++ nixos/link.nix
@@ -58,6 +58,7 @@
   systemd.tmpfiles.rules = [
     "d /srv/slskd 0770 tunnel users -"
     "d /srv/grocy 0770 tunnel users -"
+    "d /srv/jdownloader 0770 tunnel users -"
   ];
   virtualisation.oci-containers = {
     backend = "docker";
@@ -68,6 +69,16 @@
         ports = ["9283:80"];
         volumes = [
           "/srv/grocy:/app"
+        ];
+      };
+      jdownloader = {
+        autoStart = true;
+        image = "jlesage/jdownloader-2:latest";
+        ports = ["5800:5800"];
+        # user = "tunnel:users";
+        # TODO find some universal way to declare these paths like my music library so that I can use a variable
+        volumes = [
+          "/media/Data/ImportMusic/JDownloader/:/output"
         ];
       };
       slskd = {
