39a8daa517ed88c3cca69e8f3bc7e615e780f7f6
Multipixelone <finn@cnwr.net>
2024-06-14 13:12:12 -0400

    restructure

--- home/secrets.nix
+++ home/secrets.nix
@@ -3,20 +3,24 @@
   osConfig,
   ...
 }: {
-  age.identityPaths = [
-    "/home/tunnel/.ssh/agenix"
-  ];
-  age.secretsDir = "/home/tunnel/.secrets";
-  age.secrets."scrobblehome" = {
-    file = "${inputs.secrets}/media/scrobble.age";
-  };
-  age.secrets."spotify" = {
-    file = "${inputs.secrets}/media/spotify.age";
-  };
-  age.secrets."restic/passwordhome" = {
-    file = "${inputs.secrets}/restic/password.age";
-  };
-  age.secrets."restic/rclone" = {
-    file = "${inputs.secrets}/restic/${osConfig.networking.hostName}rclone.age";
+  age = {
+    identityPaths = [
+      "/home/tunnel/.ssh/agenix"
+    ];
+    secretsDir = "/home/tunnel/.secrets";
+    secrets = {
+      "scrobblehome" = {
+        file = "${inputs.secrets}/media/scrobble.age";
+      };
+      "spotify" = {
+        file = "${inputs.secrets}/media/spotify.age";
+      };
+      "restic/passwordhome" = {
+        file = "${inputs.secrets}/restic/password.age";
+      };
+      "restic/rclone" = {
+        file = "${inputs.secrets}/restic/${osConfig.networking.hostName}rclone.age";
+      };
+    };
   };
 }
