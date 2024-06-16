65bc6c9edc54292b23d65fee1febbbbb47eb2af6
Multipixelone <finn@cnwr.net>
2024-06-16 02:44:04 -0400

    add many extensions

--- home/programs/browser/default.nix
+++ home/programs/browser/default.nix
@@ -1,6 +1,24 @@
 {pkgs, ...}: let
   extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+    # blockers
+    sponsorblock
     ublock-origin
+    # privacy extensions
+    privacy-badger
+    privacy-possum
+    decentraleyes
+    link-cleaner
+    clearurls
+    fastforward
+    # ui
+    tree-style-tab
+    # utility
+    augmented-steam
+    darkreader
+    zotero-connector
+    violentmonkey # TODO add last.fm script
+    # TODO last.fm unscrobbler
+    # TODO readwise
   ];
 in {
   imports = [];
