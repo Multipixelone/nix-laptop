9a96168bc8fe2b5eada4b017b2a2efd12ee4aec8
Multipixelone <finn@cnwr.net>
2024-06-16 01:35:18 -0400

    switch to swww

--- home/programs/hyprland/modules/hyprpaper.nix
+++ home/programs/hyprland/modules/hyprpaper.nix
@@ -1,25 +1,23 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}: {
-  xdg.configFile."hypr/hyprpaper.conf".text = ''
-    preload = ${config.theme.wallpaper}
-    preload = ${config.theme.side-wallpaper}
-    wallpaper = eDP-1, ${config.theme.wallpaper}
-    wallpaper = DP-1,${config.theme.wallpaper}
-    wallpaper = DP-3,${config.theme.side-wallpaper}
-    splash = false
-  '';
-
-  systemd.user.services.hyprpaper = {
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    swww
+  ];
+  # xdg.configFile."hypr/hyprpaper.conf".text = ''
+  #   preload = ${config.theme.wallpaper}
+  #   preload = ${config.theme.side-wallpaper}
+  #   wallpaper = eDP-1, ${config.theme.wallpaper}
+  #   wallpaper = DP-1,${config.theme.wallpaper}
+  #   wallpaper = DP-3,${config.theme.side-wallpaper}
+  #   splash = false
+  # '';
+  #
+  systemd.user.services.swww = {
     Unit = {
-      Description = "Hyprland wallpaper daemon";
+      Description = "swww wallpaper daemon";
       PartOf = ["graphical-session.target"];
     };
     Service = {
-      ExecStart = "${lib.getExe pkgs.hyprpaper}";
+      ExecStart = "${pkgs.swww}/bin/swww-daemon";
       Restart = "on-failure";
     };
     Install.WantedBy = ["graphical-session.target"];
