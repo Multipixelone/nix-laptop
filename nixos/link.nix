1facf22d6152ca3f2714ca0250a0d038f2789546
Multipixelone <finn@cnwr.net>
2024-06-21 11:17:38 -0400

    force rebuild

--- nixos/link.nix
+++ nixos/link.nix
@@ -103,6 +103,7 @@ in {
           build_admins = "True";
           build_time_zones = "True";
           build_tar = "True";
+          force_rebuild = "True";
         };
       };
       slskd = {
