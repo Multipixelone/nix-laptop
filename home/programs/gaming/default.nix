2b95a554ef1bc03f4001a489ce330493062e3f89
Multipixelone <finn@cnwr.net>
2024-06-27 10:54:37 -0400

    move shell applications into desktop files

--- home/programs/gaming/default.nix
+++ home/programs/gaming/default.nix
@@ -17,21 +17,39 @@
     cemu
     # TODO make these something better than jank shell scripts
     # WINE SHELL RUNNERS! :)
-    (writeShellApplication {
-      name = "cities-skylines-2";
-      runtimeInputs = [inputs.nix-gaming.packages.${pkgs.system}.wine-tkg];
-      text = ''
-        export WINEPREFIX=/media/TeraData/Games/cities-skylines-ii
-        wine "$WINEPREFIX/drive_c/Program Files/Cities Skylines II/Cities2.exe"
-      '';
-    })
-    (writeShellApplication {
-      name = "stray";
-      runtimeInputs = [inputs.nix-gaming.packages.${pkgs.system}.wine-ge];
-      text = ''
-        export WINEPREFIX=/media/TeraData/Games/stray
-        wine "$WINEPREFIX/drive_c/Stray/Hk_project/Binaries/Win64/Stray-Win64-Shipping.exe"
-      '';
-    })
   ];
+  xdg.desktopEntries = {
+    cities-skylines-2 = {
+      name = "Cities Skylines II";
+      exec =
+        pkgs.writeShellApplication {
+          name = "cities-skylines-2";
+          runtimeInputs = [pkgs.gamemode config.programs.mangohud.package inputs.nix-gaming.packages.${pkgs.system}.wine-tkg];
+          text = ''
+            export WINEPREFIX=/media/TeraData/Games/cities-skylines-ii
+            gamemoderun mangohud wine "$WINEPREFIX/drive_c/Program Files/Cities Skylines II/Cities2.exe"
+          '';
+        }
+        + "/bin/cities-skylines-2";
+      terminal = false;
+      type = "Application";
+      categories = ["Game"];
+    };
+    stray = {
+      name = "Stray";
+      exec =
+        pkgs.writeShellApplication {
+          name = "stray";
+          runtimeInputs = [inputs.nix-gaming.packages.${pkgs.system}.wine-ge];
+          text = ''
+            export WINEPREFIX=/media/TeraData/Games/stray
+            gamemoderun mangohud wine "$WINEPREFIX/drive_c/Stray/Hk_project/Binaries/Win64/Stray-Win64-Shipping.exe"
+          '';
+        }
+        + "/bin/stray";
+      terminal = false;
+      type = "Application";
+      categories = ["Game"];
+    };
+  };
 }
