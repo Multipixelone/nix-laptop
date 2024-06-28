4c3ebebde63f790c1bf79b61ebe3ea1b8ba4272e
Multipixelone <finn@cnwr.net>
2024-06-28 17:22:41 -0400

    TEMP: vlc wayland fix

--- home/programs/media/default.nix
+++ home/programs/media/default.nix
@@ -17,7 +17,16 @@
     strawberry
     plexamp
     imv
-    vlc
+    # FIX qt doesn't honor QT_QPA_PLATFORM if DISPLAY is set??
+    (pkgs.symlinkJoin {
+      name = "vlc";
+      paths = [pkgs.vlc];
+      buildInputs = [pkgs.makeWrapper];
+      postBuild = ''
+        wrapProgram $out/bin/vlc \
+          --unset DISPLAY
+      '';
+    })
     blanket
     pavucontrol
     nicotine-plus
