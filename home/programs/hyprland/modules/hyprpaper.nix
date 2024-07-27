ef06abcc17d81db274fb0aa5fc8d3326f9d775a5
Multipixelone <finn@cnwr.net>
2024-07-27 00:08:29 -0400

    swww set wallpaper for each monitor

--- home/programs/hyprland/modules/hyprpaper.nix
+++ home/programs/hyprland/modules/hyprpaper.nix
@@ -8,7 +8,10 @@
   swww-daemon = pkgs.swww + "/bin/swww-daemon";
 in {
   wayland.windowManager.hyprland.settings = {
-    exec-once = ["${swww} img ${wallpaper}"];
+    exec-once = [
+      "${swww} img -o DP-1 --transition-fps 240 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper}"
+      "${swww} img -o DP-3 --transition-fps 60 --transition-type wave --transition-angle 120 --transition-step 30 ${config.theme.side-wallpaper}"
+    ];
   };
   systemd.user.services.swww = {
     Unit = {
