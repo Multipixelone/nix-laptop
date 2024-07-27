8351d01f2fe68e7b88fb3148aa80de66b8fe7ba1
Multipixelone <finn@cnwr.net>
2024-07-26 20:48:14 -0400

    respell gamemode-start

--- nixos/modules/gamemode.nix
+++ nixos/modules/gamemode.nix
@@ -28,7 +28,7 @@
         hyprctl --batch 'keyword animations:enabled 0; keyword decoration:drop_shadow 0; keyword decoration:blur:enabled 0; keyword general:gaps_in 0; keyword general:gaps_out 0; keyword general:border_size 1; keyword decoration:rounding 0'
       '';
     }
-    + "/bin/gamemode=start";
+    + "/bin/gamemode-start";
   endscript =
     pkgs.writeShellApplication {
       name = "gamemode-end";
