76115f5d6bf95ab5308ead322be312e923e02fe4
Multipixelone <finn@cnwr.net>
2024-07-28 17:05:22 -0400

    fix transition fps for laptop

--- home/programs/hyprland/modules/hyprpaper.nix
+++ home/programs/hyprland/modules/hyprpaper.nix
@@ -9,7 +9,7 @@
 in {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      "${swww} img -o eDP-1 --transition-fps 240 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper}"
+      "${swww} img -o eDP-1 --transition-fps 60 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper}"
       "${swww} img -o DP-1 --transition-fps 240 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper}"
       "${swww} img -o DP-3 --transition-fps 60 --transition-type wave --transition-angle 120 --transition-step 30 ${config.theme.side-wallpaper}"
     ];
