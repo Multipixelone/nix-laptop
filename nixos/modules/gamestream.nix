b70b4560491642283c2e188105ba53da003d6e76
Multipixelone <finn@cnwr.net>
2024-07-28 00:47:43 -0400

    replace string concat with sensible outPath

--- nixos/modules/gamestream.nix
+++ nixos/modules/gamestream.nix
@@ -13,42 +13,38 @@
     url,
     hash,
   }: let image = pkgs.fetchurl {inherit url hash;}; in pkgs.runCommand "${lib.nameFromURL url "."}.png" {} ''${pkgs.imagemagick}/bin/convert ${image} -background none -gravity center -extent 600x800 $out'';
-  streammon =
-    pkgs.writeShellApplication {
-      name = "streammon";
-      runtimeInputs = [pkgs.findutils pkgs.gawk pkgs.coreutils pkgs.procps pkgs.curl pkgs.hyprland];
+  streammon = pkgs.writeShellApplication {
+    name = "streammon";
+    runtimeInputs = [pkgs.findutils pkgs.gawk pkgs.coreutils pkgs.procps pkgs.curl pkgs.hyprland];
 
-      text = ''
-        width=''${1:-3840}
-        height=''${2:-2160}
-        refresh_rate=''${3:-60}
-        mon_string="DP-1,''${width}x''${height}@''${refresh_rate},1200x0,2"
-        # Unlock PC (so I don't have to type password on Steam Deck)
-        pkill -USR1 hyprlock || true
-        #curl -X 'PUT' 'http://link.bun-hexatonic.ts.net:8888/api/scenes' -H 'Content-Type: application/json' -d '{"id": "gaming-mode", "action": "activate"}'
-        systemctl --user stop hypridle
-        hyprctl keyword monitor "DP-3,disable"
-        hyprctl keyword monitor "$mon_string"
-      '';
-    }
-    + "/bin/streammon";
-  undo-command =
-    pkgs.writeShellApplication {
-      name = "undo-command";
-      runtimeInputs = [pkgs.findutils pkgs.gawk pkgs.coreutils pkgs.curl pkgs.hyprland];
+    text = ''
+      width=''${1:-3840}
+      height=''${2:-2160}
+      refresh_rate=''${3:-60}
+      mon_string="DP-1,''${width}x''${height}@''${refresh_rate},1200x0,2"
+      # Unlock PC (so I don't have to type password on Steam Deck)
+      pkill -USR1 hyprlock || true
+      #curl -X 'PUT' 'http://link.bun-hexatonic.ts.net:8888/api/scenes' -H 'Content-Type: application/json' -d '{"id": "gaming-mode", "action": "activate"}'
+      systemctl --user stop hypridle
+      hyprctl keyword monitor "DP-3,disable"
+      hyprctl keyword monitor "$mon_string"
+    '';
+  };
+  undo-command = pkgs.writeShellApplication {
+    name = "undo-command";
+    runtimeInputs = [pkgs.findutils pkgs.gawk pkgs.coreutils pkgs.curl pkgs.hyprland];
 
-      text = ''
-        mon_string="DP-1,2560x1440@240,1200x0,1"
-        #curl -X 'PUT' 'http://link.bun-hexatonic.ts.net:8888/api/scenes' -H 'Content-Type: application/json' -d '{"id": "main-purple", "action": "activate"}'
-        systemctl --user start hypridle
-        hyprctl keyword monitor "DP-3,1920x1200@60,0x0,1,transform,1"
-        hyprctl keyword monitor "$mon_string"
-      '';
-    }
-    + "/bin/undo-command";
+    text = ''
+      mon_string="DP-1,2560x1440@240,1200x0,1"
+      #curl -X 'PUT' 'http://link.bun-hexatonic.ts.net:8888/api/scenes' -H 'Content-Type: application/json' -d '{"id": "main-purple", "action": "activate"}'
+      systemctl --user start hypridle
+      hyprctl keyword monitor "DP-3,1920x1200@60,0x0,1,transform,1"
+      hyprctl keyword monitor "$mon_string"
+    '';
+  };
   prep = {
-    do = "${sh} -c \"${streammon} \${SUNSHINE_CLIENT_WIDTH} \${SUNSHINE_CLIENT_HEIGHT} \${SUNSHINE_CLIENT_FPS}\"";
-    undo = "${sh} -c \"${undo-command}\"";
+    do = "${sh} -c \"${streammon.outPath} \${SUNSHINE_CLIENT_WIDTH} \${SUNSHINE_CLIENT_HEIGHT} \${SUNSHINE_CLIENT_FPS}\"";
+    undo = "${sh} -c \"${undo-command.outPath}\"";
   };
   # TODO I wrote this while high as fuck so i think i wrote it like actually so jank LMFAO absolutely ghoulish use of string concatenation
   steam-kill = {
