9b53c855d98674ea46bb7046eddedb0fd42b0903
Multipixelone <finn@cnwr.net>
2024-08-02 13:54:09 -0400

    assign workspaces to correct monitors

--- home/programs/hyprland/conf/workspaces.nix
+++ home/programs/hyprland/conf/workspaces.nix
@@ -3,12 +3,12 @@
     workspace = [
       "5,gapsin:10,gapsout:20"
       "4, on-created-empty: obsidian,gapsin:0,gapsout:0,border:false,rounding:false"
-      "workspace = 1,monitor: DP-1,default:true"
-      "workspace = 2,monitor: DP-1"
-      "workspace = 3,monitor: DP-1"
-      "workspace = 4,monitor: DP-1"
-      "workspace = 5,monitor: DP-3"
-      "workspace = 6,monitor: DP-3"
+      "1,monitor:DP-1,default:true"
+      "2,monitor:DP-1"
+      "3,monitor:DP-1"
+      "4,monitor:DP-1"
+      "5,monitor:DP-3"
+      "6,monitor:DP-1"
     ];
   };
 }
