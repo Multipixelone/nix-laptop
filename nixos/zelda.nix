dddcd5876499b5a0e1b4d77cb21e0e101445bc96
Multipixelone <finn@cnwr.net>
2024-08-02 19:17:29 -0400

    restructure so i'm repeating less

--- nixos/zelda.nix
+++ nixos/zelda.nix
@@ -15,13 +15,17 @@
       ];
     };
   };
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-intel"];
-  boot.extraModulePackages = [];
-  boot.loader.efi = {
-    efiSysMountPoint = "/boot";
-    canTouchEfiVariables = true;
+  boot = {
+    initrd = {
+      availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
+      kernelModules = [];
+    };
+    kernelModules = ["kvm-intel"];
+    extraModulePackages = [];
+    loader.efi = {
+      efiSysMountPoint = "/boot";
+      canTouchEfiVariables = true;
+    };
   };
   # Syncthing
   services.syncthing = {
@@ -76,36 +80,41 @@
       };
     };
   };
-  networking.networkmanager.enable = true;
-  networking.hostName = "zelda";
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/576fdcd4-d642-4229-9073-90724eb72043";
-    fsType = "btrfs";
-    options = ["subvol=@" "compress=zstd:4" "ssd" "relatime" "discard=async"];
+  networking = {
+    networkmanager.enable = true;
+    useDHCP = lib.mkDefault true;
+    hostName = "zelda";
   };
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/576fdcd4-d642-4229-9073-90724eb72043";
-    fsType = "btrfs";
-    options = ["subvol=@home" "compress=zstd:4" "ssd" "relatime" "discard=async"];
-  };
-  fileSystems."/home/tunnel/Music/Library" = {
-    device = "/dev/disk/by-uuid/576fdcd4-d642-4229-9073-90724eb72043";
-    fsType = "btrfs";
-    options = ["subvol=@music" "compress=zstd:4" "ssd" "relatime" "discard=async"];
-  };
-  fileSystems."/home/tunnel/tmp" = {
-    device = "none";
-    fsType = "tmpfs";
-    options = ["defaults" "size=2G" "mode=755"];
-  };
-  fileSystems."/media/Data/Music" = {
-    device = "/dev/disk/by-uuid/576fdcd4-d642-4229-9073-90724eb72043";
-    fsType = "btrfs";
-    options = ["subvol=@music" "compress=zstd:4" "ssd" "relatime" "discard=async"];
-  };
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/6164-2046";
-    fsType = "vfat";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/576fdcd4-d642-4229-9073-90724eb72043";
+      fsType = "btrfs";
+      options = ["subvol=@" "compress=zstd:4" "ssd" "relatime" "discard=async"];
+    };
+    "/home" = {
+      device = "/dev/disk/by-uuid/576fdcd4-d642-4229-9073-90724eb72043";
+      fsType = "btrfs";
+      options = ["subvol=@home" "compress=zstd:4" "ssd" "relatime" "discard=async"];
+    };
+    "/home/tunnel/Music/Library" = {
+      device = "/dev/disk/by-uuid/576fdcd4-d642-4229-9073-90724eb72043";
+      fsType = "btrfs";
+      options = ["subvol=@music" "compress=zstd:4" "ssd" "relatime" "discard=async"];
+    };
+    "/home/tunnel/tmp" = {
+      device = "none";
+      fsType = "tmpfs";
+      options = ["defaults" "size=2G" "mode=755"];
+    };
+    "/media/Data/Music" = {
+      device = "/dev/disk/by-uuid/576fdcd4-d642-4229-9073-90724eb72043";
+      fsType = "btrfs";
+      options = ["subvol=@music" "compress=zstd:4" "ssd" "relatime" "discard=async"];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/6164-2046";
+      fsType = "vfat";
+    };
   };
 
   swapDevices = [];
@@ -114,7 +123,6 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
