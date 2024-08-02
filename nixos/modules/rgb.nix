d1b51abdca8166dd8e7c1f4afbf7d83844995b0f
Multipixelone <finn@cnwr.net>
2024-08-02 13:52:05 -0400

    disable openrgb

--- nixos/modules/rgb.nix
+++ nixos/modules/rgb.nix
@@ -4,7 +4,7 @@
   boot.kernelModules = ["i2c-dev" "i2c-piix4"];
   # hardware.openrazer.enable = true;
   services.hardware.openrgb = {
-    enable = true;
+    enable = false;
     package = pkgs.openrgb-with-all-plugins;
   };
 }
