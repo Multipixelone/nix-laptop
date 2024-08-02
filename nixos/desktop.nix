27dbfd6b5d87fc35da269b706d6f353de46b23d6
Multipixelone <finn@cnwr.net>
2024-08-02 12:39:08 -0400

    commented out code for scheduler testing

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -64,14 +64,20 @@
       };
     };
   };
+  # testing different schedulers
+  # chaotic.scx = {
+  #   enable = true;
+  #   scheduler = "scx_lavd";
+  #   # scheduler = "scx_rustland";
+  # };
   boot = {
     plymouth.enable = false;
     consoleLogLevel = 3;
     initrd.verbose = false;
     kernelParams = ["rd.udev.log_level=3" "udev.log_priority=3" "boot.shell_on_fail" "SYSTEMD_CGROUP_ENABLE_LEGACY_FORCE=1"];
     # xanmod or zen
-    # kernelPackages = pkgs.linuxPackages_xanmod;
     kernelPackages = pkgs.linuxPackages_zen;
+    # kernelPackages = pkgs.linuxPackages_cachyos;
   };
   hardware = {
     bluetooth = {
