8964c707426ef62a558047ba7ecac11376ed6bcc
Multipixelone <finn@cnwr.net>
2024-08-04 19:14:50 -0400

    add Fastfox

--- home/programs/browser/default.nix
+++ home/programs/browser/default.nix
@@ -21,6 +21,12 @@
     # TODO last.fm unscrobbler
     # TODO readwise
   ];
+  better-fox = pkgs.fetchFromGitHub {
+    owner = "yokoffing";
+    repo = "Betterfox";
+    rev = "128.0";
+    hash = "sha256-Xbe9gHO8Kf9C+QnWhZr21kl42rXUQzqSDIn99thO1kE=";
+  };
   settings = {
     "app.update.channel" = "default";
     "browser.link.open_newwindow" = true;
@@ -36,6 +42,9 @@ in {
       default = {
         inherit extensions settings;
         id = 0;
+        extraConfig = builtins.concatStringsSep "\n" [
+          (builtins.readFile "${better-fox}/Fastfox.js")
+        ];
       };
     };
   };
