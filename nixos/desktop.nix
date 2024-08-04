ffa51bd6fab2e3b6197fc443101502a6b9dc80bc
Multipixelone <finn@cnwr.net>
2024-08-04 11:48:50 -0400

    cgroup comment

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -83,7 +83,7 @@ in {
       "rd.udev.log_level=3"
       "udev.log_priority=3"
       "boot.shell_on_fail"
-      "SYSTEMD_CGROUP_ENABLE_LEGACY_FORCE=1"
+      "SYSTEMD_CGROUP_ENABLE_LEGACY_FORCE=1" # remove when i'm not booting w legacy c groups... idk why its happening and I can't be bothered to track back what package is setting it
     ];
     # xanmod or zen
     kernelPackages = pkgs.linuxPackages_xanmod_latest;
