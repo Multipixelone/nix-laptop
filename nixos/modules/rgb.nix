b94580f7458fe935bd6c8cf4002a3dc41434bd8a
Multipixelone <finn@cnwr.net>
2024-08-08 21:32:27 -0400

    comment out unused boot param

--- nixos/modules/rgb.nix
+++ nixos/modules/rgb.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   programs.coolercontrol.enable = true;
-  boot.kernelParams = ["acpi_enforce_resources=lax"];
+  # boot.kernelParams = ["acpi_enforce_resources=lax"];
   # hardware.openrazer.enable = true;
   services.hardware.openrgb = {
     enable = false;
