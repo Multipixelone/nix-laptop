c9a9511ea106c5dc3c7267691c552ebb8953b63c
Multipixelone <finn@cnwr.net>
2024-08-08 21:32:21 -0400

    remove setting set by openrgb upstream

--- nixos/modules/rgb.nix
+++ nixos/modules/rgb.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   programs.coolercontrol.enable = true;
   boot.kernelParams = ["acpi_enforce_resources=lax"];
-  boot.kernelModules = ["i2c-dev" "i2c-piix4"];
   # hardware.openrazer.enable = true;
   services.hardware.openrgb = {
     enable = false;
