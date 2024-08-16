c96e85fe8c6102e4b686f24ceed863bed499ca71
Multipixelone <finn@cnwr.net>
2024-08-16 14:50:16 -0400

    move order of swww commands

--- home/programs/hyprland/modules/hyprpaper.nix
+++ home/programs/hyprland/modules/hyprpaper.nix
@@ -9,9 +9,9 @@
 in {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      "${swww} img -o eDP-1 --transition-fps 60 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper}"
       "${swww} img -o DP-1 --transition-fps 240 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper}"
       "${swww} img -o DP-3 --transition-fps 60 --transition-type wave --transition-angle 120 --transition-step 30 ${config.theme.side-wallpaper}"
+      "${swww} img -o eDP-1 --transition-fps 60 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper}"
     ];
   };
   systemd.user.services.swww = {
