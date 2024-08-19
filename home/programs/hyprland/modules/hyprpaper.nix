1259294341c86006a488490e0b775daa10df6ed0
Multipixelone <finn@cnwr.net>
2024-08-19 11:42:59 -0400

    replace wallpaper with ShellApplication

--- home/programs/hyprland/modules/hyprpaper.nix
+++ home/programs/hyprland/modules/hyprpaper.nix
@@ -6,13 +6,18 @@
 }: let
   swww = lib.getExe pkgs.swww;
   swww-daemon = lib.getExe' pkgs.swww "swww-daemon";
+  wallpaper-set = pkgs.writeShellApplication {
+    name = "wallpaper-set";
+    runtimeInputs = [pkgs.swww];
+    text = ''
+      swww img -o eDP-1 --transition-fps 60 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper} &> /dev/null
+      swww img -o DP-1 --transition-fps 240 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper} &> /dev/null
+      swww img -o DP-3 --transition-fps 60 --transition-type wave --transition-angle 120 --transition-step 30 ${config.theme.side-wallpaper} &> /dev/null
+    '';
+  };
 in {
   wayland.windowManager.hyprland.settings = {
-    exec-once = [
-      "${swww} img -o DP-1 --transition-fps 240 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper}"
-      "${swww} img -o DP-3 --transition-fps 60 --transition-type wave --transition-angle 120 --transition-step 30 ${config.theme.side-wallpaper}"
-      "${swww} img -o eDP-1 --transition-fps 60 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper}"
-    ];
+    exec-once = [(lib.getExe wallpaper-set)];
   };
   systemd.user.services.swww = {
     Unit = {
