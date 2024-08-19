22a3111a147b0e178d5c03de6326c6ba6a0b3480
Multipixelone <finn@cnwr.net>
2024-08-19 16:37:25 -0400

    add bufferline keybindings

--- home/programs/terminal/vim/plugs/ui/bufferline.nix
+++ home/programs/terminal/vim/plugs/ui/bufferline.nix
@@ -1,6 +1,78 @@
 {
   plugins.bufferline = {
     enable = true;
-    separatorStyle = "thick"; # “slant”, “padded_slant”, “slope”, “padded_slope”, “thick”, “thin”
+    separatorStyle = "thin"; # “slant”, “padded_slant”, “slope”, “padded_slope”, “thick”, “thin”
+    keymaps = [
+      {
+        mode = "n";
+        key = "<Tab>";
+        action = "<cmd>BufferLineCycleNext<cr>";
+        options = {
+          desc = "Cycle to next buffer";
+        };
+      }
+
+      {
+        mode = "n";
+        key = "<S-Tab>";
+        action = "<cmd>BufferLineCyclePrev<cr>";
+        options = {
+          desc = "Cycle to previous buffer";
+        };
+      }
+
+      {
+        mode = "n";
+        key = "<S-l>";
+        action = "<cmd>BufferLineCycleNext<cr>";
+        options = {
+          desc = "Cycle to next buffer";
+        };
+      }
+
+      {
+        mode = "n";
+        key = "<S-h>";
+        action = "<cmd>BufferLineCyclePrev<cr>";
+        options = {
+          desc = "Cycle to previous buffer";
+        };
+      }
+
+      {
+        mode = "n";
+        key = "<leader>bd";
+        action = "<cmd>bdelete<cr>";
+        options = {
+          desc = "Delete buffer";
+        };
+      }
+
+      {
+        mode = "n";
+        key = "<leader>bb";
+        action = "<cmd>e #<cr>";
+        options = {
+          desc = "Switch to Other Buffer";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>bp";
+        action = "<cmd>BufferLineTogglePin<cr>";
+        options = {
+          desc = "Toggle pin";
+        };
+      }
+
+      {
+        mode = "n";
+        key = "<leader>bP";
+        action = "<Cmd>BufferLineGroupClose ungrouped<CR>";
+        options = {
+          desc = "Delete non-pinned buffers";
+        };
+      }
+    ];
   };
 }
