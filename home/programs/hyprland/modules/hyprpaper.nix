38b683885c00b78f9d7f6b2c921dd622576c1d9d
Multipixelone <finn@cnwr.net>
2024-08-24 12:18:20 -0400

    remove stderr pipes

--- home/programs/hyprland/modules/hyprpaper.nix
+++ home/programs/hyprland/modules/hyprpaper.nix
@@ -8,9 +8,9 @@
     name = "wallpaper-set";
     runtimeInputs = [pkgs.swww];
     text = ''
-      swww img -o eDP-1 --transition-fps 60 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper} &> /dev/null
-      swww img -o DP-1 --transition-fps 240 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper} &> /dev/null
-      swww img -o DP-3 --transition-fps 60 --transition-type wave --transition-angle 120 --transition-step 30 ${config.theme.side-wallpaper} &> /dev/null
+      swww img -o eDP-1 --transition-fps 60 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper}
+      swww img -o DP-1 --transition-fps 240 --transition-type wave --transition-angle 60 --transition-step 30 ${config.theme.wallpaper}
+      swww img -o DP-3 --transition-fps 60 --transition-type wave --transition-angle 120 --transition-step 30 ${config.theme.side-wallpaper}
     '';
   };
 in {
