faa980136443ccc72a4d27b0abd525b5d6a88660
Multipixelone <finn@cnwr.net>
2024-08-27 14:23:49 -0400

    createList for workspace hotkeys

--- home/programs/hyprland/conf/binds.nix
+++ home/programs/hyprland/conf/binds.nix
@@ -29,46 +29,45 @@ in {
     '';
     settings = {
       "$mod" = "ALT";
-      bind = [
-        "ALT_SHIFT, Q, killactive"
-        "$mod, RETURN, exec, ${terminal}"
-        "ALT_SHIFT, W, exec, firefox"
-        "ALT_SHIFT, D, exec, discord"
-        "ALT_SHIFT, S, exec, steam"
-        "ALT_SHIFT, C, exec, code"
-        "ALT_SHIFT, E, exec, pypr toggle music"
-        ", Print, exec, ${lib.getExe pkgs.grimblast} --notify --cursor copysave output"
-        "ALT , Print, exec, ${screenshot-area}"
-        "$mod, SPACE, exec, ${launcher}"
-        "$mod, M, exit"
-        "$mod, N, exec, systemctl suspend"
-        "$mod, V, togglefloating"
-        "SUPER, F, fullscreen"
-        "ALT, Tab, workspace, previous"
-        "$mod, T, exec, pypr toggle term"
-        "$mod, B, exec, pypr toggle volume"
-        "$mod, H, exec, pypr toggle helvum"
-        "Control_L&Alt_L, K, exec, pypr toggle password"
-        #", swipe:3:ld, exec, pypr toggle music"
-        "$mod, 1, workspace, 1"
-        "$mod, 2, workspace, 2"
-        "$mod, 3, workspace, 3"
-        "$mod, 4, workspace, 4"
-        "$mod, 5, workspace, 5"
-        "$mod, 6, workspace, 6"
-        "$mod, 7, workspace, 7"
-        "$mod, 8, workspace, 8"
-        "$mod, 9, workspace, 9"
-        "$mod SHIFT, 1, movetoworkspacesilent, 1"
-        "$mod SHIFT, 2, movetoworkspacesilent, 2"
-        "$mod SHIFT, 3, movetoworkspacesilent, 3"
-        "$mod SHIFT, 4, movetoworkspacesilent, 4"
-        "$mod SHIFT, 5, movetoworkspacesilent, 5"
-        "$mod SHIFT, 6, movetoworkspacesilent, 6"
-        "$mod SHIFT, 7, movetoworkspacesilent, 7"
-        "$mod SHIFT, 8, movetoworkspacesilent, 8"
-        "$mod SHIFT, 9, movetoworkspacesilent, 9"
-      ];
+      bind =
+        [
+          "ALT_SHIFT, Q, killactive"
+          "$mod, RETURN, exec, ${terminal}"
+          "ALT_SHIFT, W, exec, firefox"
+          "ALT_SHIFT, D, exec, discord"
+          "ALT_SHIFT, S, exec, steam"
+          "ALT_SHIFT, C, exec, code"
+          "ALT_SHIFT, E, exec, pypr toggle music"
+          ", Print, exec, ${lib.getExe pkgs.grimblast} --notify --cursor copysave output"
+          "ALT , Print, exec, ${screenshot-area}"
+          "$mod, SPACE, exec, ${launcher}"
+          "$mod, M, exit"
+          "$mod, N, exec, systemctl suspend"
+          "$mod, V, togglefloating"
+          "SUPER, F, fullscreen"
+          "ALT, Tab, workspace, previous"
+          "$mod, T, exec, pypr toggle term"
+          "$mod, B, exec, pypr toggle volume"
+          # "$mod, H, exec, pypr toggle helvum"
+          "Control_L&Alt_L, K, exec, pypr toggle password"
+          #", swipe:3:ld, exec, pypr toggle music"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+          builtins.concatLists (builtins.genList (
+              x: let
+                ws = let
+                  c = (x + 1) / 10;
+                in
+                  builtins.toString (x + 1 - (c * 10));
+              in [
+                "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                "$mod SHIFT, ${ws}, movetoworkspacesilent, ${toString (x + 1)}"
+              ]
+            )
+            10)
+        );
       bindm = [
         "$mod, mouse:272, movewindow"
         "$mod, mouse:273, resizewindow"
