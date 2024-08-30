18ff0d3d44ef9e6e9838a5785558f65274405c83
Multipixelone <finn@cnwr.net>
2024-08-30 12:09:23 -0400

    add custom css

--- home/programs/hyprland/modules/anyrun.nix
+++ home/programs/hyprland/modules/anyrun.nix
@@ -1,5 +1,6 @@
 {
   inputs,
+  config,
   pkgs,
   ...
 }: {
@@ -14,14 +15,50 @@
         dictionary
         websearch
       ];
-      extraCss = '''';
-      extraConfigFiles = {
-        "dictionary.ron".text = ''
-          Config(
-            prefix: "d",
-          )
-        '';
-      };
+    };
+    extraCss = ''
+      * {
+        all: unset;
+        transition: 200ms ease-out;
+        font-family: PragmataPro Liga;
+        color: #${config.lib.stylix.colors.base05};
+      }
+      #window,
+      #match,
+      #entry,
+      #plugin,
+      #main {
+        background: transparent;
+      }
+      #main {
+        margin-top: 0.5rem;
+      }
+      #match {
+        padding: 3px;
+        border-radius: 12px;
+      }
+      #match:hover,
+      #match:selected {
+        background: #${config.lib.stylix.colors.base0E};
+        padding: 0.6rem;
+      }
+      entry#entry {
+        border-color: transparent;
+        margin-top: 0.5rem;
+      }
+      box#main {
+        background: #${config.lib.stylix.colors.base02};
+        border: 3px solid #${config.lib.stylix.colors.base0E}
+        border-radius: 10px;
+        padding: 0.3rem;
+      }
+    '';
+    extraConfigFiles = {
+      "dictionary.ron".text = ''
+        Config(
+          prefix: "d",
+        )
+      '';
     };
   };
 }
