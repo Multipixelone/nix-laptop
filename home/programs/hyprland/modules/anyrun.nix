b6eaf950c6691f834f5152fa4b92e1257ee52bd8
Multipixelone <finn@cnwr.net>
2024-08-30 11:56:52 -0400

    add anyrun plugins and basic config

--- home/programs/hyprland/modules/anyrun.nix
+++ home/programs/hyprland/modules/anyrun.nix
@@ -1,6 +1,27 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [inputs.anyrun.homeManagerModules.default];
   programs.anyrun = {
     enable = true;
+    config = {
+      width = {fraction = 0.3;};
+      hideIcons = false;
+      plugins = with inputs.anyrun.packages.${pkgs.system}; [
+        applications
+        dictionary
+        websearch
+      ];
+      extraCss = '''';
+      extraConfigFiles = {
+        "dictionary.ron".text = ''
+          Config(
+            prefix: "d",
+          )
+        '';
+      };
+    };
   };
 }
