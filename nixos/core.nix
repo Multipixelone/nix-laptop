91c806773a222b93df2ae5321ace7bfbe04d0dce
Multipixelone <finn@cnwr.net>
2024-08-30 11:48:10 -0400

    add anyrun binary cache

--- nixos/core.nix
+++ nixos/core.nix
@@ -16,6 +16,7 @@
       "http://link.bun-hexatonic.ts.net:8080/tunnel?priority=50"
       "https://cache.nixos.org"
 
+      "https://anyrun.cachix.org"
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
@@ -25,6 +26,7 @@
       "tunnel:iXswb4rlkeD2EdspWLUuZwykAz1e37hmW0KBrb91OrM="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 
+      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
