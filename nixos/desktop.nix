b197b4d47ca39ed0f2a01c770611d17af9d21b90
Multipixelone <finn@cnwr.net>
2024-08-31 15:09:35 -0400

    use ovmf in qemu

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -217,6 +217,17 @@ in {
       qemu = {
         package = pkgs.qemu_kvm;
         runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [
+            (pkgs.OVMF.override {
+              secureBoot = true;
+              tpmSupport = true;
+            })
+            .fd
+          ];
+        };
       };
     };
     waydroid.enable = true;
