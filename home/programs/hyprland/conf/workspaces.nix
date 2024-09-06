77acc66c5cfeecb1abefbaaa8a81bb6ed8556895
Multipixelone <finn@cnwr.net>
2024-09-06 16:03:23 -0400

    keep gaps and rounding in obsidian workspace

--- home/programs/hyprland/conf/workspaces.nix
+++ home/programs/hyprland/conf/workspaces.nix
@@ -2,7 +2,7 @@
   wayland.windowManager.hyprland.settings = {
     workspace = [
       "5,gapsin:5,gapsout:3"
-      "4, on-created-empty: obsidian,gapsin:0,gapsout:0,border:false,rounding:false"
+      "4, on-created-empty: obsidian,border:false"
       "1,monitor:DP-1,default:true"
       "2,monitor:DP-1"
       "3,monitor:DP-1"
