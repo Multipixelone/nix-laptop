377812ba0ff8961e17a05200b416e0987099a9ec
Multipixelone <finn@cnwr.net>
2024-09-06 16:03:32 -0400

    set default workspace on zelda

--- home/programs/hyprland/conf/workspaces.nix
+++ home/programs/hyprland/conf/workspaces.nix
@@ -4,6 +4,7 @@
       "5,gapsin:5,gapsout:3"
       "4, on-created-empty: obsidian,border:false"
       "1,monitor:DP-1,default:true"
+      "1,monitor:eDP-1,default:true"
       "2,monitor:DP-1"
       "3,monitor:DP-1"
       "4,monitor:DP-1"
