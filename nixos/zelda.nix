4efa61cc2203fdf04d08ca66f7a64bbaaf1e3737
Multipixelone <finn@cnwr.net>
2024-09-08 13:38:52 -0400

    enable autocpufreq

--- nixos/zelda.nix
+++ nixos/zelda.nix
@@ -53,6 +53,19 @@
   };
   services = {
     thermald.enable = true;
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+      };
+    };
   };
   # Syncthing
   services.syncthing = {
