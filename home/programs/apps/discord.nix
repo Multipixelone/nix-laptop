30fefcee3f4e5b82086b016183557f3ff6253c4d
Multipixelone <finn@cnwr.net>
2024-09-08 19:17:29 -0400

    add catppuccin discord theme

--- home/programs/apps/discord.nix
+++ home/programs/apps/discord.nix
@@ -1,8 +1,17 @@
-{inputs, ...}: {
+{
+  inputs,
+  config,
+  ...
+}: {
   imports = [
     inputs.nixcord.homeManagerModules.nixcord
   ];
   programs.nixcord = {
     enable = true;
+    config = {
+      themeLinks = [
+        "https://catppuccin.github.io/discord/dist/catppuccin-${config.catppuccin.flavor}-${config.catppuccin.accent}.theme.css"
+      ];
+    };
   };
 }
