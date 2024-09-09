e87efa37aa84185cfaeb2001ecbf0f914dd3c16e
Multipixelone <finn@cnwr.net>
2024-09-08 21:51:29 -0400

    switch to wine instead of umu

--- pkgs/games/bookworm-adventures/default.nix
+++ pkgs/games/bookworm-adventures/default.nix
@@ -4,7 +4,7 @@
   symlinkJoin,
   writeShellScriptBin,
   winetricks,
-  umu,
+  wine,
   proton ? "GE-Proton",
   wineFlags ? "",
   pname ? "bookworm-adventures",
@@ -37,9 +37,9 @@
     export GAMEID="umu-3470"
     export STORE="none"
 
-    PATH=${lib.makeBinPath [umu winetricks]}:$PATH
+    PATH=${lib.makeBinPath [wine winetricks]}:$PATH
     USER="$(whoami)"
-    GAME_PATH="$WINEPREFIX/drive_c/Program Files (x86)/Bookworm Adventures Deluxe"
+    GAME_PATH="$WINEPREFIX/drive_c/Program Files/Bookworm Adventures Deluxe"
     GAME_BIN="$GAME_PATH/BookwormAdventures.exe"
 
     if [ ! -d "$WINEPREFIX" ]; then
@@ -49,14 +49,15 @@
 
       # install launcher
       # Copy source folder into drive_c/Games
-      cp -r "${src}/Bookworm Adventures Deluxe" "$WINEPREFIX/drive_c/Program Files (x86)/"
+      cp -r "${src}/Bookworm Adventures Deluxe" "$WINEPREFIX/drive_c/Program Files"
     fi
 
     cd $WINEPREFIX
 
     ${preCommands}
 
-    umu-run ${wineFlags} "$GAME_BIN" "$@"
+    cd $GAME_PATH
+    wine ${wineFlags} "$GAME_BIN" "$@"
 
     ${postCommands}
   '';
