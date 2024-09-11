0a664e51588e4054594c1c2c83d432c8061b4865
Multipixelone <finn@cnwr.net>
2024-09-11 08:32:34 -0400

    latexrun: remove from default install

--- home/programs/latex/default.nix
+++ home/programs/latex/default.nix
@@ -2,7 +2,6 @@
   home.packages = with pkgs; [
     zotero
     texliveFull
-    latexrun
     # TODO Maybe move Libreoffice to some other kind of general editing module? This just seemed like the most sensible place rn
     libreoffice-fresh
     jdk # Needed for libreoffice?
