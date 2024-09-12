fafc7dcb4eb331b987af6c9680d4a5df16ca01a6
Multipixelone <finn@cnwr.net>
2024-09-12 12:37:04 -0400

    remove default monitor for DP-1
    when I have both monitors (DP-1 for link & eDP-1 for zelda) assigned to
    the default, only the first one takes effect and the deafult ws isn't
    set on zelda.

--- home/programs/hyprland/conf/workspaces.nix
+++ home/programs/hyprland/conf/workspaces.nix
@@ -3,7 +3,6 @@
     workspace = [
       "5,gapsin:5,gapsout:3"
       "4, on-created-empty: obsidian,border:false"
-      "1,monitor:DP-1,default:true"
       "1,monitor:eDP-1,default:true"
       "2,monitor:DP-1"
       "3,monitor:DP-1"
