a9c8efcbabbce295910bb7ecb30939b87b8600b7
Multipixelone <finn@cnwr.net>
2024-09-16 15:15:36 -0400

    zellij: rename tab with running process

--- home/programs/terminal/fish.nix
+++ home/programs/terminal/fish.nix
@@ -52,6 +52,30 @@ in {
     shellInit = fish-config;
     interactiveShellInit = ''
       ${lib.getExe pkgs.any-nix-shell} fish --info-right | source
+      if status is-interactive
+            if type -q zellij
+                # Update the zellij tab name with the current process name or pwd.
+                function zellij_tab_name_update_pre --on-event fish_preexec
+                    if set -q ZELLIJ
+                        set -l cmd_line (string split " " -- $argv)
+                        set -l process_name $cmd_line[1]
+                        if test -n "$process_name" -a "$process_name" != "z"
+                            command nohup zellij action rename-tab $process_name >/dev/null 2>&1
+                        end
+                    end
+                end
+
+                function zellij_tab_name_update_post --on-event fish_postexec
+                    if set -q ZELLIJ
+                        set -l cmd_line (string split " " -- $argv)
+                        set -l process_name $cmd_line[1]
+                        if test "$process_name" = "z"
+                            command nohup zellij action rename-tab (prompt_pwd) >/dev/null 2>&1
+                        end
+                    end
+                end
+            end
+        end
     '';
     functions = {
       nvimrg = "nvim -q (rg --vimgrep $argv | psub)";
