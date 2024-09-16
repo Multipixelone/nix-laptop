82af64e99fc9b5587a310414435cf50a89ce4abb
Multipixelone <finn@cnwr.net>
2024-09-16 14:12:06 -0400

    default zjstatus config & zjstatus pkg

--- home/programs/terminal/zellij.nix
+++ home/programs/terminal/zellij.nix
@@ -3,15 +3,23 @@
     enable = true;
     enableFishIntegration = true; # launches on every open of shell
   };
-  xdg.configFile."zellij/layouts/default.kdl".text = ''
-    layout {
-        default_tab_template {
-            pane size=2 borderless=true {
-                plugin location="file://${inputs.zjstatus}/bin/zjstatus.wasm" {
-                }
-            }
-            children
-        }
-    }
-  '';
+  xdg.configFile = {
+    "zellij/layouts/default.kdl".text = ''
+      layout {
+          default_tab_template {
+              pane size=2 borderless=true {
+                  plugin location="file://${pkgs.zjstatus}/bin/zjstatus.wasm" {
+                      format_left   "{mode}#[bg=#${colors.base00}] {tabs}"
+                      format_center ""
+                      format_right  "#[bg=#${colors.base00},fg=#${colors.base0D}]#[bg=#${colors.base0D},fg=#${colors.base01},bold] #[bg=#${colors.base02},fg=#${colors.base05},bold] {session} #[bg=#${colors.base03},fg=#${colors.base05},bold]"
+                      format_space  ""
+                      format_hide_on_overlength "true"
+                      format_precedence "crl"
+                  }
+              }
+              children
+          }
+      }
+    '';
+  };
 }
