72a6d51da1b3cb7d19e5ec54f1392a3a6ae6deeb
Multipixelone <finn@cnwr.net>
2024-09-16 13:59:27 -0400

    beginning configuration of zjstatus

--- home/programs/terminal/zellij.nix
+++ home/programs/terminal/zellij.nix
@@ -1,6 +1,17 @@
-{...}: {
+{inputs, ...}: {
   programs.zellij = {
     enable = true;
     enableFishIntegration = true; # launches on every open of shell
   };
+  xdg.configFile."zellij/layouts/default.kdl".text = ''
+    layout {
+        default_tab_template {
+            pane size=2 borderless=true {
+                plugin location="file://${inputs.zjstatus}/bin/zjstatus.wasm" {
+                }
+            }
+            children
+        }
+    }
+  '';
 }
