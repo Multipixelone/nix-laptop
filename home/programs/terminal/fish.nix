ed7ceb9537a4b9953c9fd519e1748c82948a9dc0
Multipixelone <finn@cnwr.net>
2024-09-20 12:03:31 -0400

    lock zellij in helix

--- home/programs/terminal/fish.nix
+++ home/programs/terminal/fish.nix
@@ -61,6 +61,9 @@ in {
                         set -l process_name $cmd_line[1]
                         if test -n "$process_name" -a "$process_name" != "z"
                             command nohup zellij action rename-tab $process_name >/dev/null 2>&1
+                            if test "$process_name" = "hx"
+                              command nohup zellij action switch-mode locked >/dev/null 2>&1
+                            end
                         end
                     end
                 end
@@ -69,6 +72,7 @@ in {
                     if set -q ZELLIJ
                         set -l cmd_line (string split " " -- $argv)
                         set -l process_name $cmd_line[1]
+                        command nohup zellij action switch-mode normal >/dev/null 2>&1
                         if test "$process_name" = "z"
                             command nohup zellij action rename-tab (prompt_pwd) >/dev/null 2>&1
                         end
