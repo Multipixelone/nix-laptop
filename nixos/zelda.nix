c0105b92db4e52092660eca8bd0aee411f777e0b
Multipixelone <finn@cnwr.net>
2024-09-23 13:35:24 -0400

    add swap file

--- nixos/zelda.nix
+++ nixos/zelda.nix
@@ -203,7 +203,7 @@
     };
   };
 
-  swapDevices = [];
+  swapDevices = [{device = "/swap/swapfile";}];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
