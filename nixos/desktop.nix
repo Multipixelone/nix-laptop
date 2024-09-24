20cc82a48d39fec1136d9e4ae329757bf6c02dbd
Multipixelone <finn@cnwr.net>
2024-09-24 17:54:41 -0400

    allow dns queries from any interface

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -143,7 +143,7 @@
     dnscrypt-proxy2 = {
       enable = true;
       settings = {
-        listen_addresses = lib.mkIf (config.networking.hostName == "link") ["0.0.0.0:53"];
+        listen_addresses = ["0.0.0.0:53"];
         ipv6_servers = true;
         require_dnssec = true;
         sources.public-resolvers = {
