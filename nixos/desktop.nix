0d91e194eac900d355762ff11460f1a6fc11d1ef
Multipixelone <finn@cnwr.net>
2024-09-24 09:54:34 -0400

    dnscrypt: run on all devices

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -125,6 +125,7 @@
       enable32Bit = true;
     };
   };
+  networking.nameservers = ["127.0.0.1" "::1"];
   services = {
     blueman.enable = true;
     flatpak.enable = true;
@@ -137,6 +138,21 @@
     psd.enable = true;
     btrfs.autoScrub.enable = true;
     printing.enable = true;
+    dnscrypt-proxy2 = {
+      enable = true;
+      settings = {
+        ipv6_servers = true;
+        require_dnssec = true;
+        sources.public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+      };
+    };
     zerotierone = {
       enable = true;
       joinNetworks = ["52b337794f640fc8"];
--- nixos/zelda.nix
+++ nixos/zelda.nix
@@ -73,21 +73,6 @@
   services = {
     thermald.enable = true;
     tlp.enable = false;
-    dnscrypt-proxy2 = {
-      enable = true;
-      settings = {
-        ipv6_servers = true;
-        require_dnssec = true;
-        sources.public-resolvers = {
-          urls = [
-            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-          ];
-          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
-          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-        };
-      };
-    };
     # # testing undervolting
     # undervolt = {
     #   enable = true;
@@ -150,7 +135,6 @@
     };
   };
   networking = {
-    nameservers = ["127.0.0.1" "::1"];
     networkmanager = {
       enable = true;
       dns = "none";
