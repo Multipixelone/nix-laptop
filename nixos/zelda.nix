d3b3dec3d6aa5dd06ee91e9ab7d68aec9a63b93e
Multipixelone <finn@cnwr.net>
2024-09-25 14:04:51 -0400

    wg: switch to wg-quick

--- nixos/zelda.nix
+++ nixos/zelda.nix
@@ -141,21 +141,17 @@
     };
     useDHCP = lib.mkDefault true;
     hostName = "zelda";
-    wireguard.interfaces.wg0 = {
-      ips = ["10.100.0.2/24"];
-      listenPort = 51628;
+    wg-quick.interfaces.wg0 = {
+      address = ["10.100.0.2/24"];
+      mtu = 1000;
+      dns = ["10.100.0.1"];
       privateKeyFile = config.age.secrets."wireguard".path;
-      postSetup = ''
-        printf "nameserver 10.100.0.1" | ${pkgs.openresolv}/bin/resolvconf -a wg0 -m 0
-      '';
-      postShutdown = ''
-        ${pkgs.openresolv}/bin/resolvconf -d wg0
-      '';
       peers = [
         {
           publicKey = "i2nI/xG1Jh3WVyOk79Lz/jH6B9SbmnocjbZv+fLoJwE=";
           allowedIPs = ["0.0.0.0/0"];
-          endpoint = "frwgq.duckdns.org:51628";
+          # endpoint = "frwgq.duckdns.org:51628";
+          endpoint = "68.237.124.182:51628";
           persistentKeepalive = 25;
         }
       ];
