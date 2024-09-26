2a96626aaa77874c87691eaf9a168f1a180f6cbc
Multipixelone <finn@cnwr.net>
2024-09-26 12:58:02 -0400

    Revert "add shadowsocks server"
    This reverts commit 4630e5ae012330f3298e492ec58ecbea1aa8576e.

--- home/zelda.nix
+++ home/zelda.nix
@@ -1,33 +1,8 @@
-{
-  pkgs,
-  inputs,
-  config,
-  ...
-}: {
+{...}: {
   imports = [
     ./desktop.nix
   ];
   wayland.windowManager.hyprland.extraConfig = ''
     env = GDK_SCALE,2
   '';
-  age.secrets = {
-    "shadowsocks-client" = {
-      file = "${inputs.secrets}/wireguard/shadowsocks-client.age";
-    };
-  };
-  systemd.user.services = {
-    shadowsocks-proxy = {
-      Unit = {
-        Description = "local shadowsocks proxy";
-        After = "network.target";
-      };
-      Install = {
-        WantedBy = ["default.target"];
-      };
-      Service = {
-        ExecStart = "${pkgs.shadowsocks-libev}/bin/ss-local -c ${config.age.secrets."shadowsocks-client".path}";
-        ExecStop = "${pkgs.toybox}/bin/killall ss-local";
-      };
-    };
-  };
 }
