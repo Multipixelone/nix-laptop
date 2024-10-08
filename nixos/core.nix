49b9349ef93d81df6660329405b6915eb133e7c5
Multipixelone <finn@cnwr.net>
2024-10-07 20:38:57 -0400

    mouse: disable autosuspend

--- nixos/core.nix
+++ nixos/core.nix
@@ -140,6 +140,8 @@
 
       # Logitech G502 Lightspeed
       SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c539", TEST=="power/control", ATTR{power/control}="on"
+      SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c539", TEST=="power/autosuspend", ATTR{power/autosuspend}="0"
+      SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c539", TEST=="power/autosuspend_delay_ms", ATTR{power/autosuspend_delay_ms}="0"
     '';
     tailscale.enable = true;
   };
