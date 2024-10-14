20d75c5f34f5b7bc43353d9ae0714325edccf033
Multipixelone <finn@cnwr.net>
2024-10-14 12:12:43 -0400

    enable openrgb

--- nixos/modules/rgb.nix
+++ nixos/modules/rgb.nix
@@ -3,7 +3,7 @@
   # boot.kernelParams = ["acpi_enforce_resources=lax"];
   # hardware.openrazer.enable = true;
   services.hardware.openrgb = {
-    enable = false;
     package = pkgs.openrgb-with-all-plugins;
+    enable = true;
   };
 }
