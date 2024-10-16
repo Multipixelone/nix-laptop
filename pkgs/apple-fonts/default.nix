0b6c03bd670a6a0f5309ba07760ba5723b9fd636
Multipixelone <finn@cnwr.net>
2024-10-16 16:59:32 -0400

    apple fonts: only install emoji

--- pkgs/apple-fonts/default.nix
+++ pkgs/apple-fonts/default.nix
@@ -6,27 +6,7 @@
 }:
 stdenv.mkDerivation rec {
   pname = "apple-fonts";
-  version = "1.1";
-
-  pro = fetchurl {
-    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
-    sha256 = "sha256-B8xljBAqOoRFXvSOkOKDDWeYUebtMmQLJ8lF05iFnXk=";
-  };
-
-  compact = fetchurl {
-    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
-    sha256 = "sha256-L4oLQ34Epw1/wLehU9sXQwUe/LaeKjHRxQAF6u2pfTo=";
-  };
-
-  mono = fetchurl {
-    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg";
-    sha256 = "sha256-Uarx1TKO7g5yVBXAx6Yki065rz/wRuYiHPzzi6cTTl8=";
-  };
-
-  ny = fetchurl {
-    url = "https://devimages-cdn.apple.com/design/resources/download/NY.dmg";
-    sha256 = "sha256-yYyqkox2x9nQ842laXCqA3UwOpUGyIfUuprX975OsLA=";
-  };
+  version = "1.2";
 
   emoji = fetchurl {
     url = "https://github.com/samuelngs/apple-emoji-linux/releases/download/v17.4/AppleColorEmoji.ttf";
@@ -40,43 +20,12 @@ stdenv.mkDerivation rec {
   dontUnpack = true;
 
   installPhase = ''
-    7z x ${pro}
-    cd SFProFonts
-    7z x 'SF Pro Fonts.pkg'
-    7z x 'Payload~'
-    mkdir -p $out/fontfiles
-    mv Library/Fonts/* $out/fontfiles
-    cd ..
-
-    7z x ${mono}
-    cd SFMonoFonts
-    7z x 'SF Mono Fonts.pkg'
-    7z x 'Payload~'
-    mv Library/Fonts/* $out/fontfiles
-    cd ..
-
-    7z x ${compact}
-    cd SFCompactFonts
-    7z x 'SF Compact Fonts.pkg'
-    7z x 'Payload~'
-    mv Library/Fonts/* $out/fontfiles
-    cd ..
-
-    7z x ${ny}
-    cd NYFonts
-    7z x 'NY Fonts.pkg'
-    7z x 'Payload~'
-    mv Library/Fonts/* $out/fontfiles
-    cp ${emoji} $out/fontfiles
-
     mkdir -p $out/share/fonts/truetype
-    # mv $out/fontfiles/*.otf $out/share/fonts/op
-    mv $out/fontfiles/*.ttf $out/share/fonts/truetype
-    rm -rf $out/fontfiles
+    cp ${emoji} $out/share/fonts/truetype
   '';
 
   meta = {
-    description = "Apple San Francisco, New York fonts";
+    description = "Apple color emoji fonts";
     homepage = "https://developer.apple.com/fonts/";
     license = lib.licenses.unfree;
   };
