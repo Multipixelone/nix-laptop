0739fda9ff08be8597dc2ddd3cb91a9275afc70a
Multipixelone <finn@cnwr.net>
2024-10-16 18:31:45 -0400

    nextmeeting follows nixpkgs

--- flake.nix
+++ flake.nix
@@ -24,8 +24,10 @@
     zjstatus.url = "github:dj95/zjstatus";
     helix.url = "github:helix-editor/helix";
     nixcord.url = "github:kaylorben/nixcord";
-    # TODO change url to upstream once the PR gets merged
-    nextmeeting.url = "github:Multipixelone/nextmeeting/reformat?dir=packaging";
+    nextmeeting = {
+      url = "github:Multipixelone/nextmeeting/reformat?dir=packaging";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     blocklist = {
       url = "github:StevenBlack/hosts";
       flake = false;
