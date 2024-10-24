a370f668699f3f2d98497eeb61ece36e27567c3d
Multipixelone <finn@cnwr.net>
2024-10-24 12:49:42 -0400

    add github token to nix extraOptions

--- nixos/core.nix
+++ nixos/core.nix
@@ -12,34 +12,45 @@
     "wireguard".file = "${inputs.secrets}/wireguard/${config.networking.hostName}.age";
     "psk".file = "${inputs.secrets}/wireguard/psk.age";
     "duckdns".file = "${inputs.secrets}/wireguard/duckdns.age";
+    "nix" = {
+      file = "${inputs.secrets}/github/nix.age";
+      mode = "400";
+      owner = "tunnel";
+      group = "users";
+    };
   };
   nixpkgs.config.allowUnfree = true;
-  nix.settings = {
-    trusted-users = ["@wheel" "root" "nix-ssh"];
-    auto-optimise-store = true;
-    experimental-features = ["nix-command" "flakes"];
-    substituters = [
-      # lowest priority (hit my cache last)
-      "https://attic-cache.fly.dev/system?priority=50"
-      "https://cache.nixos.org"
+  nix = {
+    extraOptions = ''
+      !include ${config.age.secrets.nix.path}
+    '';
+    settings = {
+      trusted-users = ["@wheel" "root" "nix-ssh"];
+      auto-optimise-store = true;
+      experimental-features = ["nix-command" "flakes"];
+      substituters = [
+        # lowest priority (hit my cache last)
+        "https://attic-cache.fly.dev/system?priority=50"
+        "https://cache.nixos.org"
 
-      "https://helix.cachix.org"
-      "https://anyrun.cachix.org"
-      "https://hyprland.cachix.org"
-      "https://nix-community.cachix.org"
-      "https://nix-gaming.cachix.org"
-    ];
-    trusted-public-keys = [
-      "system:XwpCBI5UHFzt9tEmiq3v8S062HvTqWPUwBR8PoHSfSk="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "https://helix.cachix.org"
+        "https://anyrun.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://nix-gaming.cachix.org"
+      ];
+      trusted-public-keys = [
+        "system:XwpCBI5UHFzt9tEmiq3v8S062HvTqWPUwBR8PoHSfSk="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 
-      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
-      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-    ];
-    netrc-file = config.age.secrets."attic".path;
+        "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      ];
+      netrc-file = config.age.secrets."attic".path;
+    };
   };
   # User
   users.users.tunnel = {
