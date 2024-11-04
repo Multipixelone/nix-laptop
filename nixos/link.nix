80ef646228e366864c5e747ef1cf7a7e2756b19c
Multipixelone <finn@cnwr.net>
2024-11-04 09:14:52 -0500

    uninstall blender

--- nixos/link.nix
+++ nixos/link.nix
@@ -152,7 +152,7 @@ in {
   };
   environment.systemPackages = [
     (import ./modules/scripts/sleep.nix {inherit pkgs;})
-    (pkgs.blender.override {hipSupport = true;})
+    # (pkgs.blender.override {hipSupport = true;})
   ];
   boot = {
     initrd = {
