0fe7e9ae649bb971c5a9140e2c64915ee4e31101
Multipixelone <finn@cnwr.net>
2024-11-12 12:06:20 -0500

    add right_format and fix truncation

--- home/programs/terminal/starship.nix
+++ home/programs/terminal/starship.nix
@@ -32,15 +32,33 @@
         renamed = "[◎◦](italic bright-blue)";
         deleted = "[✕](italic red)";
       };
+      git_branch = {
+        symbol = "";
+        truncation_symbol = "⋯";
+        truncation_length = 11;
+        format = "[$symbol $branch(:$remote_branch)]";
+        ignore_branches = [
+          "main"
+          "master"
+        ];
+      };
       directory = {
-        home_symbol = "⌂ ";
-        truncation_length = 10;
-        truncation_symbol = "□ ";
+        # home_symbol = "⌂ ";
+        truncation_length = 3;
+        truncation_symbol = "…/";
         read_only = " ◈";
         use_os_path_sep = true;
         format = "[$path]($style)[$read_only]($read_only_style)";
         repo_root_style = "bold blue";
         repo_root_format = "[$before_root_path]($before_repo_root_style)[$repo_root]($repo_root_style)[$path]($style)[$read_only]($read_only_style) [ ](bold bright-blue)";
+      };
+      nix_shell = {
+        style = "dimmed blue";
+        symbol = "✶";
+        format = "[$symbol $state ]($style) [$name](italic dimmed white)";
+        impure_msg = "[⌽](bold dimmed red)";
+        pure_msg = "[⌾](bold dimmed green)";
+        unknown_msg = "[◌](bold dimmed ellow)";
       };
       format = lib.concatStrings [
         # "($nix_shell$container$git_metrics)$cmd_duration"
@@ -49,7 +67,76 @@
         "$directory"
         "$git_status"
         "$git_metrics"
+        "$git_branch"
+        "$nix_shell"
         "$character"
+      ];
+      right_format = lib.concatStrings [
+        "$singularity"
+        "$kubernetes"
+        "$vcsh"
+        "$fossil_branch"
+        "$hg_branch"
+        "$pijul_channel"
+        "$docker_context"
+        "$package"
+        "$c"
+        "$cmake"
+        "$cobol"
+        "$daml"
+        "$dart"
+        "$deno"
+        "$dotnet"
+        "$elixir"
+        "$elm"
+        "$erlang"
+        "$fennel"
+        "$golang"
+        "$guix_shell"
+        "$haskell"
+        "$haxe"
+        "$helm"
+        "$java"
+        "$julia"
+        "$kotlin"
+        "$gradle"
+        "$lua"
+        "$nim"
+        "$nodejs"
+        "$ocaml"
+        "$opa"
+        "$perl"
+        "$php"
+        "$pulumi"
+        "$purescript"
+        "$python"
+        "$raku"
+        "$rlang"
+        "$red"
+        "$ruby"
+        "$rust"
+        "$scala"
+        "$solidity"
+        "$swift"
+        "$terraform"
+        "$vlang"
+        "$vagrant"
+        "$zig"
+        "$buf"
+        "$conda"
+        "$meson"
+        "$spack"
+        "$memory_usage"
+        "$aws"
+        "$gcloud"
+        "$openstack"
+        "$azure"
+        "$crystal"
+        "$custom"
+        "$status"
+        "$os"
+        "$battery"
+        "$time"
       ];
     };
   };
