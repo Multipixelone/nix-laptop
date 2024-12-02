ff74a023361478248e40c2b5d7e634fd931be2cf
Multipixelone <finn@cnwr.net>
2024-12-02 15:08:29 -0500

    Revert "remove unneeded curl patch"
    This reverts commit 03bf6ba59618ca519c8c6a9a826f4f560baa7312.

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -225,6 +225,28 @@
       (_final: prev: {
         zjstatus = inputs.zjstatus.packages.${prev.system}.default;
       })
+      (_final: prev: let
+        patched-curl = prev.curl.overrideAttrs (oldAttrs: {
+          patches =
+            (oldAttrs.patches or [])
+            ++ [
+              # https://github.com/curl/curl/issues/15496
+              (prev.fetchpatch {
+                url = "https://github.com/curl/curl/commit/f5c616930b5cf148b1b2632da4f5963ff48bdf88.patch";
+                hash = "sha256-FlsAlBxAzCmHBSP+opJVrZG8XxWJ+VP2ro4RAl3g0pQ=";
+              })
+              # https://github.com/curl/curl/issues/15513
+              (prev.fetchpatch {
+                url = "https://github.com/curl/curl/commit/0cdde0fdfbeb8c35420f6d03fa4b77ed73497694.patch";
+                hash = "sha256-WP0zahMQIx9PtLmIDyNSJICeIJvN60VzJGN2IhiEYv0=";
+              })
+            ];
+        });
+      in {
+        nix = prev.nix.override {
+          curl = patched-curl;
+        };
+      })
     ];
   };
   # Fonts
