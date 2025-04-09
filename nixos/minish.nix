4e5be073be3fba0dd6560c9080e2300580511eb9
Multipixelone <finn@cnwr.net>
2025-04-09 10:36:37 -0400

    fix hostname and stylix

--- nixos/minish.nix
+++ nixos/minish.nix
@@ -6,8 +6,17 @@
   ...
 }: {
   imports = [
-    ./desktop.nix
+    ./core.nix
   ];
+  networking.hostName = "minish";
+  stylix = {
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    polarity = "dark";
+    image = builtins.fetchurl {
+      url = "https://drive.usercontent.google.com/download?id=1OrRpU17DU78sIh--SNOVI6sl4BxE06Zi";
+      sha256 = "sha256:14nh77xn8x58693y2na5askm6612xqbll2kr6237y8pjr1jc24xp";
+    };
+  };
   # specialisation = {
   #   nvidia-sync.configuration = {
   #     imports = [
