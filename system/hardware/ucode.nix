cae7503567c4a506287c557e0e53ad69145290a8
Multipixelone <finn@cnwr.net>
2025-04-13 11:50:33 -0400

    disable amd microcode check

--- system/hardware/ucode.nix
+++ system/hardware/ucode.nix
@@ -1,4 +1,5 @@
 {config, ...}: {
+  boot.kernelParams = ["microcode.amd_sha_check=off"];
   services.ucodenix = {
     enable = config.hardware.enableRedistributableFirmware;
     cpuModelId = "00A20F10";
