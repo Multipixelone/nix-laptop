7261eef01394db3ca0036d2967570c78330fc39f
Multipixelone <finn@cnwr.net>
2025-04-14 07:07:19 -0400

    add marin hardware-conf

--- hosts/marin/default.nix
+++ hosts/marin/default.nix
@@ -1,4 +1,7 @@
 {lib, ...}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
   networking = {
@@ -11,20 +14,5 @@
       }
     ];
     defaultGateway = "192.168.7.1";
-  };
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-id/dm-uuid-LVM-gWVEE2JcjOfdXFfqFulTflOiq27lhC6ccOexhOpZXOW7qL1dBM67aCOSezPYimi9";
-      fsType = "ext4";
-      options = ["default"];
-    };
-    "/boot" = {
-      device = "/dev/disk/by-uuid/a4b3ca17-0a5d-42f8-b48c-520053b3454e";
-      fsType = "ext4";
-    };
-    "/boot/efi" = {
-      device = "/dev/disk/by-uuid/4F36-2B33";
-      fsType = "vfat";
-    };
   };
 }
