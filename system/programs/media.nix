87aeaa150524d5c8d51df1796ac26fedf8c78b8e
Multipixelone <finn@cnwr.net>
2025-04-22 22:53:15 -0400

    move out python playlist downloader

--- system/programs/media.nix
+++ system/programs/media.nix
@@ -6,26 +6,6 @@
   inputs,
   ...
 }: let
-  playlist-download-python =
-    pkgs.writers.writePython3Bin "playlist-download-python" {
-      libraries = with pkgs.python3Packages; [
-      ];
-    } ''
-      PLAYLIST_DIR = "/home/tunnel/Music/Playlists"
-      MOPIDY_DIR = "/home/tunnel/.local/share/mopidy/m3u"
-      IPOD_DIR = "/home/tunnel/Music/.ipod"
-      MUSIC_DIR = "/media/Data/Music"
-      SECRET_FILE = "${config.age.secrets."plex".path}"
-      PLAYLISTS = {"monthly playlist": 24562, # noqa
-        "forgotten faves": 48614, # noqa
-        "good listening and learning": 20340, # noqa
-        "slipped through": 26220, # noqa
-        "vgm study": 53423, # noqa
-        "amtrak": 26224, # noqa
-        "mackin mabel": 61577, # noqa
-        "summer jams": 61792, # noqa
-      } # noqa
-    '';
   playlist-download = pkgs.writeShellApplication {
     name = "playlist-download";
     runtimeInputs = [
@@ -122,7 +102,6 @@ in {
     self.packages.${pkgs.system}.spotify2musicbrainz
 
     playlist-download
-    playlist-download-python
     (pkgs.writeShellApplication {
       name = "ipod-sync";
       runtimeInputs = [pkgs.rsync];
