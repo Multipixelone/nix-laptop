0c4fe55a956c8d7364e8048078d03ac767d46af9
Multipixelone <finn@cnwr.net>
2025-04-22 22:40:14 -0400

    set music env vars on minish

--- hosts/minish/default.nix
+++ hosts/minish/default.nix
@@ -5,6 +5,12 @@
   # nh default flake
   environment.variables.FLAKE = "/home/tunnel/Documents/Git/nix-laptop";
 
+  # media env vars
+  environment.variables = {
+    MUSIC_DIR = "C:\\Users\\Finn Rutis\\Music\\Library";
+    PLAYLIST_DIR = "/mnt/c/Users/Finn Rutis/Music/Playlists";
+  };
+
   networking.hostName = "minish";
   wsl = {
     enable = true;
