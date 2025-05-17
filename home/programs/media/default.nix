aa085c9cdf984f25c1faa63f92f231163bb7aab6
Multipixelone <finn@cnwr.net>
2025-05-17 12:09:32 -0400

    move mopidy into separate file

--- home/programs/media/default.nix
+++ home/programs/media/default.nix
@@ -1,12 +1,9 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ./mpv.nix
     ./spicetify.nix
     ./beets.nix
+    ./mopidy.nix
   ];
   home.packages = with pkgs; [
     ani-cli
@@ -34,41 +31,4 @@
     #   # location = "/media/TeraData/Games/cities-skylines-ii";
     # })
   ];
-  services.mopidy = {
-    enable = true;
-    extensionPackages = with pkgs; [
-      mopidy-mpd
-      mopidy-iris
-      mopidy-mpris
-      mopidy-scrobbler
-      mopidy-local
-      # (callPackage ../../../pkgs/mopidy/spotify.nix {})
-    ];
-    extraConfigFiles = [
-      "${config.age.secrets."scrobblehome".path}"
-      # "${config.age.secrets."spotify".path}"
-    ];
-    settings = {
-      local = {
-        media_dir = ["/media/Data/Music"];
-        scan_timeout = 5000;
-      };
-      mpd = {
-        hostname = "0.0.0.0";
-      };
-      http = {
-        hostname = "0.0.0.0";
-      };
-      iris = {
-        enabled = true;
-        country = "US";
-        locale = "en_US";
-      };
-      m3u = {
-        enabled = true;
-        playlists_dir = "/home/tunnel/.local/share/mopidy/m3u";
-        base_dir = "";
-      };
-    };
-  };
 }
