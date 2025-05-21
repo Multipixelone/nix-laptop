19a71f42bdb8499475ffc8976ff66ecc74d2141c
Multipixelone <finn@cnwr.net>
2025-05-21 11:08:53 -0400

    define version for bgutil-pot-provider

--- home/programs/media/media-dl.nix
+++ home/programs/media/media-dl.nix
@@ -5,6 +5,7 @@
   config,
   ...
 }: let
+  bgutil-ytdlp-pot-provider-version = "0.8.5";
   # wrap cookies into spotdl
   spotdl-wrapped = let
     # use version of spotdl that accepts extractor-args for yt-dlp
@@ -37,7 +38,7 @@ in {
       pkgs.fetchFromGitHub {
         owner = "Brainicism";
         repo = "bgutil-ytdlp-pot-provider";
-        rev = "f16fd8527bb6203943a522d552c9cdc972553448";
+        rev = "refs/tags/${bgutil-ytdlp-pot-provider-version}";
         hash = "sha256-dRFGvBKHJxl4hIB5gBZGUyhwYZB/7KQ63DYTHzTAh4s=";
       }
       + "/plugin";
