1026afd0a9a451f8304169bdfcb8aa4c9f1bac9b
Multipixelone <finn@cnwr.net>
2025-05-20 23:55:37 -0400

    move media downloading scripts into separate file

--- home/programs/terminal/default.nix
+++ home/programs/terminal/default.nix
@@ -29,26 +29,6 @@
     --client-secret=$(cat ${config.age.secrets."gcalsecret".path}) \
     $@
   '';
-  # wrap cookies into spotdl
-  spotdl-wrapped = let
-    # use version of spotdl that accepts extractor-args for yt-dlp
-    spotdl-args = pkgs.spotdl.overrideAttrs {
-      src = pkgs.fetchFromGitHub {
-        owner = "Kyrluckechuck";
-        repo = "spotify-downloader";
-        rev = "ad48f5ca55b2cc79e3b83bc8e8aa16b16c11d485";
-        hash = "sha256-56zVTlzJnI7AFmcZ9ZNZPDx9wc5JxFnSWxmrPkr4bWI=";
-      };
-    };
-  in
-    pkgs.writeShellScriptBin "spotdl" ''
-      ${lib.getExe spotdl-args} \
-      --cookie-file ${config.age.secrets."yt-dlp".path} \
-      --yt-dlp-args "extractor-args \"youtube:player_client=web_music,default;getpot_bgutil_baseurl=http://127.0.0.1:4416\"" \
-      --format opus \
-      --bitrate disable \
-      $@
-    '';
 in {
   imports = [
     ./btop.nix
@@ -56,6 +36,7 @@ in {
     ./fish.nix
     ./yazi.nix
     ./ncmpcpp.nix
+    ./media-dl.nix
     ./helix.nix
     ./shell-script.nix
     ./zellij.nix
@@ -78,28 +59,6 @@ in {
       "gcalsecret" = {
         file = "${inputs.secrets}/gcal/secret.age";
       };
-      "yt-dlp" = {
-        file = "${inputs.secrets}/media/ytdlp.age";
-        # yt-dlp needs write access to cookie file for some reason?
-        mode = "600";
-      };
-    };
-  };
-  xdg.configFile = {
-    "yt-dlp/plugins/bgutil-ytdlp-pot-provider".source =
-      pkgs.fetchFromGitHub {
-        owner = "Brainicism";
-        repo = "bgutil-ytdlp-pot-provider";
-        rev = "7b3900cb908f1de922b2b98b7d4319e12df496a7";
-        hash = "sha256-E448uUDnXpUo/DvGjFGbIEHDS98ngGOufJ173hIwwaM=";
-      }
-      + "/plugin";
-    "yt-dlp/plugins/yt-dlp-get-pot".source = pkgs.fetchFromGitHub rec {
-      version = "0.3.0";
-      owner = "coletdjnz";
-      repo = "yt-dlp-get-pot";
-      tag = "v${version}";
-      hash = "sha256-MtQFXWJByo/gyftMtywCCfpf8JtldA2vQP8dnpLEl7U=";
     };
   };
   home.packages = with pkgs; [
@@ -119,7 +78,6 @@ in {
     tgpt-wrapped
     todoist-wrapped
     gcalcli-wrapped
-    spotdl-wrapped
   ];
   home.shell.enableFishIntegration = true;
   programs = {
@@ -190,22 +148,7 @@ in {
         };
       };
     };
-    yt-dlp = {
-      enable = true;
-      settings = {
-        embed-thumbnail = true;
-        embed-metadata = true;
-        embed-chapters = true;
-        embed-subs = true;
-        sponsorblock-mark = "all";
-        sponsorblock-remove = "sponsor";
-        downloader = "aria2c";
-        downloader-args = "aria2c:'-c -x8 -s8 -k1M'";
-        cookies = config.age.secrets."yt-dlp".path;
-      };
-    };
     navi.enable = true;
-    aria2.enable = true;
     ripgrep.enable = true;
     gh = {
       enable = true;
---
+++ home/programs/terminal/media-dl.nix
@@ -0,0 +1,69 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  config,
+  ...
+}: let
+  # wrap cookies into spotdl
+  spotdl-wrapped = let
+    # use version of spotdl that accepts extractor-args for yt-dlp
+    spotdl-args = pkgs.spotdl.overrideAttrs {
+      src = pkgs.fetchFromGitHub {
+        owner = "Kyrluckechuck";
+        repo = "spotify-downloader";
+        rev = "ad48f5ca55b2cc79e3b83bc8e8aa16b16c11d485";
+        hash = "sha256-56zVTlzJnI7AFmcZ9ZNZPDx9wc5JxFnSWxmrPkr4bWI=";
+      };
+    };
+  in
+    pkgs.writeShellScriptBin "spotdl" ''
+      ${lib.getExe spotdl-args} \
+      --cookie-file ${config.age.secrets."yt-dlp".path} \
+      --yt-dlp-args "extractor-args \"youtube:player_client=web_music,default;getpot_bgutil_baseurl=http://127.0.0.1:4416\"" \
+      --format opus \
+      --bitrate disable \
+      $@
+    '';
+in {
+  age.secrets."yt-dlp" = {
+    file = "${inputs.secrets}/media/ytdlp.age";
+    # yt-dlp needs write access to cookie file for some reason?
+    mode = "600";
+  };
+  xdg.configFile = {
+    "yt-dlp/plugins/bgutil-ytdlp-pot-provider".source =
+      pkgs.fetchFromGitHub {
+        owner = "Brainicism";
+        repo = "bgutil-ytdlp-pot-provider";
+        rev = "7b3900cb908f1de922b2b98b7d4319e12df496a7";
+        hash = "sha256-E448uUDnXpUo/DvGjFGbIEHDS98ngGOufJ173hIwwaM=";
+      }
+      + "/plugin";
+    "yt-dlp/plugins/yt-dlp-get-pot".source = pkgs.fetchFromGitHub rec {
+      version = "0.3.0";
+      owner = "coletdjnz";
+      repo = "yt-dlp-get-pot";
+      tag = "v${version}";
+      hash = "sha256-MtQFXWJByo/gyftMtywCCfpf8JtldA2vQP8dnpLEl7U=";
+    };
+  };
+  home.packages = [spotdl-wrapped];
+  programs = {
+    aria2.enable = true;
+    yt-dlp = {
+      enable = true;
+      settings = {
+        embed-thumbnail = true;
+        embed-metadata = true;
+        embed-chapters = true;
+        embed-subs = true;
+        sponsorblock-mark = "all";
+        sponsorblock-remove = "sponsor";
+        downloader = lib.getExe config.programs.aria2.package;
+        downloader-args = "aria2c:'-c -x8 -s8 -k1M'";
+        cookies = config.age.secrets."yt-dlp".path;
+      };
+    };
+  };
+}
