a990932ebc7ddfba84f863ed163cf517bbad267e
Multipixelone <finn@cnwr.net>
2025-05-22 15:04:34 -0400

    enable mpdscribble

--- home/programs/media/mpd.nix
+++ home/programs/media/mpd.nix
@@ -1,12 +1,21 @@
 {
   config,
+  inputs,
   ...
 }: {
+  age.secrets."scribblepw".file = "${inputs.secrets}/media/lastfmpw.age";
   services = {
     mpd = {
       enable = true;
       playlistDirectory = config.home.sessionVariables.PLAYLIST_DIR;
       musicDirectory = config.home.sessionVariables.MUSIC_DIR;
+    };
+    mpdscribble = {
+      enable = true;
+      endpoints."last.fm" = {
+        username = "Tunnelmaker";
+        passwordFile = config.age.secrets."scribblepw".path;
+      };
     };
   };
 }
