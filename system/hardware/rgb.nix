09c2f93904118f78f0888b4faa648214ac0873a0
Multipixelone <finn@cnwr.net>
2025-06-22 15:42:47 -0400

    openrgb dev version

--- system/hardware/rgb.nix
+++ system/hardware/rgb.nix
@@ -5,6 +5,19 @@
   # hardware.openrazer.enable = true;
   services.hardware.openrgb = {
     enable = true;
-    package = pkgs.openrgb;
+    package = pkgs.openrgb.overrideAttrs {
+      version = "1.0";
+      src = pkgs.fetchFromGitLab {
+        owner = "CalcProgrammer1";
+        repo = "OpenRGB";
+        rev = "ce32b8801001992796dde69ce5a4b0f1f1e1d504";
+        hash = "sha256-/DusVAfTYmWR6oiFAVKAENaqx5V2kwjgkGtFAVnGeRA=";
+      };
+      postPatch = ''
+        patchShebangs scripts/build-udev-rules.sh
+        substituteInPlace scripts/build-udev-rules.sh \
+          --replace-fail "/usr/bin/env chmod" "${pkgs.coreutils}/bin/chmod"
+      '';
+    };
   };
 }
