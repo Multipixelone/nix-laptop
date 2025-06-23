131a72a79bf086c0ce7bc9082d1e4a252a4ca26d
Multipixelone <finn@cnwr.net>
2025-06-23 10:51:42 -0400

    change snapserver address

--- hosts/link/snapclient.nix
+++ hosts/link/snapclient.nix
@@ -3,14 +3,14 @@
   pkgs,
   ...
 }: {
-  user.services = {
+  systemd.user.services = {
     snapclient = {
       description = "SnapCast client";
       after = ["pipewire.service"];
       wants = ["pipewire.service"];
       wantedBy = ["multi-user.target"];
       serviceConfig = {
-        ExecStart = "${lib.getExe' pkgs.snapcast "snapclient"} --host 192.168.3.127 --player pulse";
+        ExecStart = "${lib.getExe' pkgs.snapcast "snapclient"} --host 192.168.5.21 --player pulse";
       };
     };
   };
