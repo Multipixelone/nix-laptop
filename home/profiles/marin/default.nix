ff5023b6dea3c4859c0a18fb9bb8e6e9afdd93fd
Multipixelone <finn@cnwr.net>
2025-06-26 11:07:15 -0400

    add plexamp headless quadlet

---
+++ home/profiles/marin/default.nix
@@ -0,0 +1,25 @@
+{
+  virtualisation.quadlet.containers.plexamp-headless = {
+    autoStart = true;
+    serviceConfig = {
+      RestartSec = "10";
+      Restart = "always";
+    };
+    containerConfig = {
+      image = "ghcr.io/multipixelone/plexamp:amd64";
+      volumes = [
+        "/srv/plexamp:/root/.local/share/Plexamp/Settings"
+        "/run/user/1000/pipewire-0:/tmp/pipewire-0"
+        # "/run/dbus/system_bus_socket:/run/dbus/system_bus_socket"
+      ];
+      environments = {
+        XDG_RUNTIME_DIR = "/tmp";
+      };
+      publishPorts = [
+        "0.0.0.0:32400:32400"
+        "0.0.0.0:32500:32500"
+        "0.0.0.0:20000:20000"
+      ];
+    };
+  };
+}
