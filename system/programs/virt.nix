9e7ff0ba8e3cbd965709dda6baef58a0232a423b
Multipixelone <finn@cnwr.net>
2025-07-02 13:45:14 -0400

    switch to overlay storage driver

--- system/programs/virt.nix
+++ system/programs/virt.nix
@@ -29,6 +29,9 @@
       dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true;
     };
+    containers.storage.settings.storage = {
+      driver = "overlay";
+    };
     # FIX figure out why rootless podman isn't working. Replace with docker for now
     # docker = {
     #   autoPrune.enable = true;
