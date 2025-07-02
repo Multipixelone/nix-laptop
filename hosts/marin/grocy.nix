d4f2278c998ac757700f8fd23ee1643445e45583
Multipixelone <finn@cnwr.net>
2025-07-02 13:03:36 -0400

    add grocy server

---
+++ hosts/marin/grocy.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  inputs,
+  ...
+}: {
+  age.secrets."grocy".file = "${inputs.secrets}/grocy.age";
+  virtualisation.oci-containers.containers.barcode-buddy = {
+    autoStart = false;
+    image = "f0rc3/barcodebuddy:latest";
+    ports = ["7575:80"];
+    environment.Grocy__BaseUrl = "localhost";
+    environmentFiles = [
+      config.age.secrets."grocy".path
+    ];
+  };
+  services.grocy = {
+    enable = true;
+    hostName = "192.168.5.21";
+    nginx.enableSSL = false;
+  };
+}
