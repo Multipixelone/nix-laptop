54a2d0b1f21b4ac12cca5ffdcf305a696d9343e4
Multipixelone <finn@cnwr.net>
2025-07-02 13:45:09 -0400

    slskd -> nicotine+ vnc

--- system/services/slskd.nix
+++ system/services/slskd.nix
@@ -2,25 +2,24 @@
   systemd.tmpfiles.rules = [
     "d /srv/slskd 0770 tunnel users -"
   ];
-  virtualisation.oci-containers.containers.slskd = {
-    autoStart = true;
-    image = "slskd/slskd:0.22.5";
-    ports = ["5030:5030" "2234:2234"];
-    user = "1000:100";
+  virtualisation.oci-containers.containers.nicotine = {
+    autoStart = false;
+    image = "ghcr.io/fletchto99/nicotine-plus-docker:latest";
+    ports = [
+      "5030:6080"
+      "2234:2234"
+    ];
+    # user = "1000:100";
     # TODO find some universal way to declare these paths like my music library so that I can use a variable
     volumes = [
-      "/srv/slskd:/app"
-      "/media/Data/Music/:/music"
-      "/media/Data/ImportMusic/slskd/:/downloads"
-      "/media/Data/ImportMusic/InProgress:/incomplete"
+      "/srv/slskd:/config"
+      "/media/Data/Music/:/data/shared"
+      "/media/Data/ImportMusic/slskd/:/data/downloads"
+      "/media/Data/ImportMusic/InProgress:/data/incomplete_incomplete"
     ];
     environment = {
-      # TODO add soulseek password as agenix secret
-      SLSKD_REMOTE_CONFIGURATION = "true";
-      SLSKD_SHARED_DIR = "/music";
-      SLSKD_DOWNLOAD_DIR = "/downloads";
-      SLSKD_INCOMPLETE_DIR = "/incomplete";
-      SLSKD_SLSK_LISTEN_PORT = "2234";
+      PUID = "1000";
+      PGID = "100";
     };
   };
   security = {
@@ -29,7 +28,7 @@
       extraConfig = ''
         polkit.addRule(function(action, subject) {
             if (action.id == "org.freedesktop.systemd1.manage-units") {
-                if (action.lookup("unit") == "podman-slskd.service") {
+                if (action.lookup("unit") == "podman-nicotine.service") {
                     var verb = action.lookup("verb");
                     if (verb == "start" || verb == "stop") {
                         return polkit.Result.YES;
