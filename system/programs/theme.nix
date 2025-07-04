7c1055c6932e98c6570f0ce829ad299d85580f96
Multipixelone <finn@cnwr.net>
2025-07-04 01:26:38 -0400

    disable grub theming stylix

--- system/programs/theme.nix
+++ system/programs/theme.nix
@@ -24,6 +24,9 @@
     autoEnable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     polarity = "dark";
+    targets = {
+      grub.enable = false;
+    };
     image = builtins.fetchurl {
       url = "https://drive.usercontent.google.com/download?id=1OrRpU17DU78sIh--SNOVI6sl4BxE06Zi";
       sha256 = "sha256:14nh77xn8x58693y2na5askm6612xqbll2kr6237y8pjr1jc24xp";
