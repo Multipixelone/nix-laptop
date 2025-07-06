94aa64b2a51aaa668be402a56e237ee254b394ca
Multipixelone <finn@cnwr.net>
2025-07-06 14:56:53 -0400

    reformat & add zjstatus-hints

--- home/programs/terminal/zellij.nix
+++ home/programs/terminal/zellij.nix
@@ -5,17 +5,17 @@
 }: let
   inherit (config.lib.stylix) colors;
   zjstatus-conf = ''
-    plugin location="file://${pkgs.zjstatus}/bin/zjstatus.wasm" {
-        format_left   "{mode} {tabs}"
-        format_center "{notifications}"
-        format_right  "{pipe_zjstatus_hints}#[fg=#${colors.base05},bold] {session}"
+    plugin location="zjstatus" {
+        format_left   "{mode}{tabs} #[fg=#6C7086,italic]@{session} {notifications}"
+        format_center ""
+        format_right  "{pipe_zjstatus_hints}#[fg=#${colors.base05},bold]"
         format_space  ""
         format_hide_on_overlength "true"
-        format_precedence "crl"
+        format_precedence "lrc"
 
         notification_format_unread           "#[fg=#89B4FA,blink]  #[fg=#89B4FA] {message} "
         notification_format_no_notifications "#[fg=#494d64,dim]"
-        notification_show_interval           "5"
+        notification_show_interval           "7"
 
         pipe_zjstatus_hints_format "#[bg=#cba6f7,fg=#1e1e2e,bold] {output} "
 
@@ -24,20 +24,20 @@
         border_format   "#[fg=#6C7086]{char}"
         border_position "top"
 
-        mode_normal        "#[bg=#${colors.base0B},fg=#${colors.base02},bold] NORMAL#[fg=#${colors.base0B}]"
-        mode_locked        "#[bg=#${colors.base04},fg=#${colors.base02},bold] LOCKED #[fg=#${colors.base04}]"
-        mode_resize        "#[bg=#${colors.base08},fg=#${colors.base02},bold] RESIZE#[fg=#${colors.base08}]"
-        mode_pane          "#[bg=#${colors.base0D},fg=#${colors.base02},bold] PANE#[fg=#${colors.base0D}]"
-        mode_tab           "#[bg=#${colors.base07},fg=#${colors.base02},bold] TAB#[fg=#${colors.base07}]"
-        mode_scroll        "#[bg=#${colors.base0A},fg=#${colors.base02},bold] SCROLL#[fg=#${colors.base0A}]"
-        mode_enter_search  "#[bg=#${colors.base0D},fg=#${colors.base02},bold] ENT-SEARCH#[fg=#${colors.base0D}]"
-        mode_search        "#[bg=#${colors.base0D},fg=#${colors.base02},bold] SEARCHARCH#[fg=#${colors.base0D}]"
-        mode_rename_tab    "#[bg=#${colors.base07},fg=#${colors.base02},bold] RENAME-TAB#[fg=#${colors.base07}]"
-        mode_rename_pane   "#[bg=#${colors.base0D},fg=#${colors.base02},bold] RENAME-PANE#[fg=#${colors.base0D}]"
-        mode_session       "#[bg=#${colors.base0E},fg=#${colors.base02},bold] SESSION#[fg=#${colors.base0E}]"
-        mode_move          "#[bg=#${colors.base0F},fg=#${colors.base02},bold] MOVE#[fg=#${colors.base0F}]"
-        mode_prompt        "#[bg=#${colors.base0D},fg=#${colors.base02},bold] PROMPT#[fg=#${colors.base0D}]"
-        mode_tmux          "#[bg=#${colors.base09},fg=#${colors.base02},bold] TMUX#[fg=#${colors.base09}]"
+        mode_normal        ""
+        mode_locked        "#[fg=#${colors.base04}]#[bg=#${colors.base04},fg=#${colors.base02},bold]LOCKED #[fg=#${colors.base04}] "
+        mode_resize        "#[fg=#${colors.base08}]#[bg=#${colors.base08},fg=#${colors.base02},bold]RESIZE#[fg=#${colors.base08}] "
+        mode_pane          "#[fg=#${colors.base0D}]#[bg=#${colors.base0D},fg=#${colors.base02},bold]PANE#[fg=#${colors.base0D}] "
+        mode_tab           "#[fg=#${colors.base07}]#[bg=#${colors.base07},fg=#${colors.base02},bold]TAB#[fg=#${colors.base07}] "
+        mode_scroll        "#[fg=#${colors.base0A}]#[bg=#${colors.base0A},fg=#${colors.base02},bold]SCROLL#[fg=#${colors.base0A}] "
+        mode_enter_search  "#[fg=#${colors.base0D}]#[bg=#${colors.base0D},fg=#${colors.base02},bold]ENT-SEARCH#[fg=#${colors.base0D}] "
+        mode_search        "#[fg=#${colors.base0D}]#[bg=#${colors.base0D},fg=#${colors.base02},bold]SEARCHARCH#[fg=#${colors.base0D}] "
+        mode_rename_tab    "#[fg=#${colors.base07}]#[bg=#${colors.base07},fg=#${colors.base02},bold]RENAME-TAB#[fg=#${colors.base07}] "
+        mode_rename_pane   "#[fg=#${colors.base0D}]#[bg=#${colors.base0D},fg=#${colors.base02},bold]RENAME-PANE#[fg=#${colors.base0D}] "
+        mode_session       "#[fg=#${colors.base0E}]#[bg=#${colors.base0E},fg=#${colors.base02},bold]SESSION#[fg=#${colors.base0E}] "
+        mode_move          "#[fg=#${colors.base0F}]#[bg=#${colors.base0F},fg=#${colors.base02},bold]MOVE#[fg=#${colors.base0F}] "
+        mode_prompt        "#[fg=#${colors.base0D}]#[bg=#${colors.base0D},fg=#${colors.base02},bold]PROMPT#[fg=#${colors.base0D}] "
+        mode_tmux          "#[fg=#${colors.base09}]#[bg=#${colors.base09},fg=#${colors.base02},bold]TMUX#[fg=#${colors.base09}] "
 
         // formatting for inactive tabs
         tab_normal              "#[fg=#${colors.base0D}]#[bg=#${colors.base0D},fg=#${colors.base02},bold]{index} #[bg=#${colors.base02},fg=#${colors.base05},bold] {name}{floating_indicator}#[fg=#${colors.base02},bold]"
@@ -74,15 +74,15 @@ in {
   };
   xdg.configFile = {
     "zellij/config.kdl".text = ''
-      simplified_ui true
       pane_frames false
       show_startup_tips false
       plugins {
+          zjstatus location="file://${pkgs.zjstatus}/bin/zjstatus.wasm"
           zjstatus-hints location="file://${pkgs.zjstatus-hints}/bin/zjstatus-hints.wasm" {
-            max_length 0 // 0 = unlimited
-            overflow_str "..." // default
-            pipe_name "zjstatus_hints" // default
-            hide_in_base_mode false // default
+            max_length 0
+            overflow_str "..."
+            pipe_name "zjstatus_hints"
+            hide_in_base_mode false
         }
       }
       load_plugins {
