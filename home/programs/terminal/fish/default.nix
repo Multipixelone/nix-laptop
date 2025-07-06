8490f193079fb211489446ab1a036916281f7343
Multipixelone <finn@cnwr.net>
2025-07-06 12:36:46 -0400

    send notification to zellij with done

--- home/programs/terminal/fish/default.nix
+++ home/programs/terminal/fish/default.nix
@@ -8,6 +8,10 @@
   fzf-config = ''
     set -x FZF_DEFAULT_OPTS "--preview='bat {} --color=always'" \n
     set -x SKIM_DEFAULT_COMMAND "rg --files || fd || find ."
+    set -U __done_notification_command "zellij pipe \"zjstatus::notify::\$title \$message\""
+    set -U __done_allow_nongraphical 1
+    set -U --append __done_exclude '^lazygit'
+    set -U --append __done_exclude '^hx'
   '';
   # pure-config = ''
   #   set pure_enable_single_line_prompt true
