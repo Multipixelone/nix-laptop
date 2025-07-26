a851da7d12970bf796c68179d00b23f14a1d3721
Multipixelone <finn@cnwr.net>
2025-07-26 07:48:22 -0400

    add prismlauncher sub

--- system/nix/substituters.nix
+++ system/nix/substituters.nix
@@ -11,6 +11,7 @@
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
+      "https://prismlauncher.cachix.org"
     ];
     trusted-public-keys = [
       "system:XwpCBI5UHFzt9tEmiq3v8S062HvTqWPUwBR8PoHSfSk="
@@ -22,6 +23,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
     ];
   };
 }
