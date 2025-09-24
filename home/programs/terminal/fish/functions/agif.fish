21a03e80f07de00c3c37b21898fb073b7fdc4053
Multipixelone <finn@cnwr.net>
2025-09-24 17:09:23 -0400

    shorten function name

--- home/programs/terminal/fish/functions/agif.fish
+++ home/programs/terminal/fish/functions/agif.fish
@@ -1,4 +1,4 @@
-function yt-dlp-gif --argument url -d 'Download a YouTube video and save it as a gif'
+function agif --argument url -d 'Download a YouTube video and save it as a gif for anki'
 
     yt-dlp -f bestvideo -t mp4 "$url" -o - | ffmpeg -i pipe: -an -filter:v "fps=30,scale=1920x1080:flags=bicublin" -f yuv4mpegpipe - | gifski -o output.gif --fps 30 --quality 85 --lossy-quality=30 -
 
