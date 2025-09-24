8652470569f8999349ea05353ecee8a13c1b5f3c
Multipixelone <finn@cnwr.net>
2025-09-24 09:42:39 -0400

    add yt-dlp-gif function

---
+++ home/programs/terminal/fish/functions/yt-dlp-gif.fish
@@ -0,0 +1,6 @@
+function yt-dlp-gif --argument url -d 'Download a YouTube video and save it as a gif'
+
+    yt-dlp -f bestvideo -t mp4 "$url" -o - | ffmpeg -i pipe: -an -filter:v "fps=30,scale=1920x1080:flags=bicublin" -f yuv4mpegpipe - | gifski -o output.gif --fps 30 --quality 85 --lossy-quality=30 -
+
+    echo "Successfully downloaded $url"
+end
