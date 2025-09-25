50d7af53e156739fc8d0f471373c695d654eaabf
Multipixelone <finn@cnwr.net>
2025-09-25 12:41:56 -0400

    don't download thumbnail

--- home/programs/terminal/fish/functions/agif.fish
+++ home/programs/terminal/fish/functions/agif.fish
@@ -1,6 +1,6 @@
 function agif --argument url -d 'Download a YouTube video and save it as a gif for anki'
 
-    yt-dlp -f bestvideo -t mp4 "$url" -o - | ffmpeg -i pipe: -an -filter:v "fps=30,scale=1920x1080:flags=bicublin" -f yuv4mpegpipe - | gifski -o output.gif --fps 30 --quality 85 --lossy-quality=30 -
+    yt-dlp --no-write-thumbnail -f bestvideo -t mp4 "$url" -o - | ffmpeg -i pipe: -an -filter:v "fps=30,scale=1920x1080:flags=bicublin" -f yuv4mpegpipe - | gifski -o output.gif --fps 30 --quality 85 --lossy-quality=30 -
 
     echo "Successfully downloaded $url"
 end
