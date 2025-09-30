af672f00aa65efbdac7bc5ce90b267b075139b82
Multipixelone <finn@cnwr.net>
2025-09-29 20:11:15 -0400

    allowInsecurePredicate fix

--- hosts/marin/hardware-configuration.nix
+++ hosts/marin/hardware-configuration.nix
@@ -18,9 +18,10 @@
     extraModulePackages = [config.boot.kernelPackages.broadcom_sta];
   };
 
-  nixpkgs.config.permittedInsecurePackages = [
-    "broadcom-sta-6.30.223.271-57-6.12.45"
-  ];
+  nixpkgs.config.allowInsecurePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "broadcom-sta" # aka “wl”
+    ];
 
   fileSystems = {
     "/" = {
