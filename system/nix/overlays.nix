18af58853048ebcd9aa793ae2cac3a24d1a807c9
Multipixelone <finn@cnwr.net>
2025-10-05 23:26:07 -0400

    temp: build sunshine from prerelease

--- system/nix/overlays.nix
+++ system/nix/overlays.nix
@@ -43,40 +43,38 @@
       #     ];
       #   };
       # })
-      # (self: super: {
-      #   # temporary until there's a new release that contains https://github.com/LizardByte/Sunshine/pull/3783
-      #   sunshine = super.sunshine.overrideAttrs {
-      #     inherit (pins.sunshine) version;
-      #     src = pins.sunshine;
+      (self: super: {
+        # temporary until https://github.com/NixOS/nixpkgs/pull/446141 is merged
+        sunshine = super.sunshine.overrideAttrs (
+          final: prev: {
+            inherit (pins.sunshine) version;
+            src = pins.sunshine;
+            cmakeFlags = prev.cmakeFlags ++ [
+              (lib.cmakeFeature "SYSTEMD_MODULES_LOAD_DIR" "lib/modules-load.d")
+            ];
 
-      #     # they changed the name of the .desktop file so I just copied both of the sections and hard coded the new .desktop file name
-      #     postPatch = ''
-      #       # remove upstream dependency on systemd and udev
-      #       substituteInPlace cmake/packaging/linux.cmake \
-      #         --replace-fail 'find_package(Systemd)' "" \
-      #         --replace-fail 'find_package(Udev)' ""
+            ui = pkgs.buildNpmPackage {
+              inherit (final) version src;
+              pname = "sunshine-ui";
+              npmDepsHash = "sha256-KUzJLwdZhs3BFoWTGWhUy1sxQgY8OUrgBtumnHnMjPI=";
 
-      #       # don't look for npm since we build webui separately
-      #       substituteInPlace cmake/targets/common.cmake \
-      #         --replace-fail 'find_program(NPM npm REQUIRED)' ""
+              # use generated package-lock.json as upstream does not provide one
+              postPatch = ''
+                cp ${./package-lock.json} ./package-lock.json
+              '';
 
-      #       substituteInPlace packaging/linux/dev.lizardbyte.app.Sunshine.desktop \
-      #         --subst-var-by PROJECT_NAME 'Sunshine' \
-      #         --subst-var-by PROJECT_DESCRIPTION 'Self-hosted game stream host for Moonlight' \
-      #         --subst-var-by SUNSHINE_DESKTOP_ICON 'sunshine' \
-      #         --subst-var-by CMAKE_INSTALL_FULL_DATAROOTDIR "$out/share" \
-      #         --replace-fail '/usr/bin/env systemctl start --u sunshine' 'sunshine'
+              installPhase = ''
+                runHook preInstall
 
-      #       substituteInPlace packaging/linux/sunshine.service.in \
-      #         --subst-var-by PROJECT_DESCRIPTION 'Self-hosted game stream host for Moonlight' \
-      #         --subst-var-by SUNSHINE_EXECUTABLE_PATH $out/bin/sunshine \
-      #         --replace-fail '/bin/sleep' '${self.lib.getExe' self.coreutils "sleep"}'
-      #     '';
+                mkdir -p "$out"
+                cp -a . "$out"/
 
-      #     postInstall = ''
-      #       install -Dm644 ../packaging/linux/dev.lizardbyte.app.Sunshine.desktop $out/share/applications/dev.lizardbyte.app.Sunshine.desktop
-      #     '';
-      #   };
-      # })
+                runHook postInstall
+              '';
+            };
+
+          }
+        );
+      })
     ];
 }
