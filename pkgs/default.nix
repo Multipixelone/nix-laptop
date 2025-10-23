1031f2fb9f67034df10279e37810dcd998bc9d8f
Multipixelone <finn@cnwr.net>
2025-10-23 16:39:04 -0400

    remove bandcamp-dl (unused)

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -13,7 +13,7 @@
           pragmata = pkgs.callPackage ./pragmata { };
           moondeck = pkgs.qt6.callPackage ./moondeck args;
           slskd-stats = pkgs.python3Packages.callPackage ./slskd-stats args;
-          bandcamp-dl = pkgs.python3Packages.callPackage ./bandcamp-dl args;
+          # bandcamp-dl = pkgs.python3Packages.callPackage ./bandcamp-dl args;
           izotope = pkgs.callPackage ./izotope { };
           subway = pkgs.callPackage ./aequilibrae/gtfs-subway.nix { };
         };
