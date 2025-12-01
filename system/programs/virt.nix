fa2e4a4556aad5577e17edd079b0058c6af1e420
Multipixelone <finn@cnwr.net>
2025-12-01 09:54:34 -0500

    shutdown libvirt on shutdown

--- system/programs/virt.nix
+++ system/programs/virt.nix
@@ -20,6 +20,8 @@
     # oci-containers.backend = "docker";
     libvirtd = {
       enable = true;
+      onBoot = "ignore";
+      onShutdown = "shutdown";
       qemu = {
         package = pkgs.qemu_kvm;
         runAsRoot = true;
