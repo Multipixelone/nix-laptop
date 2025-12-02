4871b4cc4ff88bd4f4054021deefa31071c88faa
Multipixelone <finn@cnwr.net>
2025-12-01 23:26:51 -0500

    gamemode: try out amd_performance_level

--- system/programs/gamemode.nix
+++ system/programs/gamemode.nix
@@ -78,6 +78,11 @@ in
           renice = 15;
           inhibit_screensaver = 0;
         };
+        gpu = {
+          apply_gpu_optimisations = "accept-responsibility";
+          gpu_device = 1;
+          amd_performance_level = "high";
+        };
         custom = {
           start = lib.getExe startscript;
           end = lib.getExe endscript;
