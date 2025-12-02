1c5c90481246ff1fb169fd3902b543718df2ce20
Multipixelone <finn@cnwr.net>
2025-12-01 23:31:07 -0500

    remove unneeded imports

--- home/profiles/zelda/default.nix
+++ home/profiles/zelda/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ../../desktop.nix
+    ../../zelda.nix
   ];
 }
--- home/zelda.nix
+++ home/zelda.nix
@@ -1,11 +1,5 @@
 { ... }:
 {
-  imports = [
-    ./desktop.nix
-  ];
-  wayland.windowManager.hyprland.extraConfig = ''
-    env = GDK_SCALE,2
-  '';
   programs.looking-glass-client = {
     enable = true;
     settings = {
