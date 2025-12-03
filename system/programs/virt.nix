898f03f11f6af67dd8928c91268a9e91311eafc7
Multipixelone <finn@cnwr.net>
2025-12-03 00:43:27 -0500

    performance gov qemu hook

--- system/programs/virt.nix
+++ system/programs/virt.nix
@@ -3,6 +3,34 @@
   programs.virt-manager.enable = true;
   networking.firewall.trustedInterfaces = [ "virbr0" ];
   environment.systemPackages = [ pkgs.distrobox ];
+  environment.etc."libvirt/hooks/qemu" = {
+    source = pkgs.writeText "qemu" ''
+      #!/usr/bin/env bash
+
+      OBJECT="$1"
+      OPERATION="$2"
+      SUB_OPERATION="$3"
+      EXTRA_ARG="$4"
+
+      if [[ "$OBJECT" == "win11" ]]; then
+          CORES=$(seq 0 15)
+
+          if [[ "$OPERATION" == "prepare" ]]; then
+              echo "Hook: Setting performance governor for $OBJECT"
+              for core in $CORES; do
+                  echo performance > /sys/devices/system/cpu/cpu$core/cpufreq/scaling_governor
+              done
+
+          elif [[ "$OPERATION" == "release" ]]; then
+              echo "Hook: Reverting to powersave governor for $OBJECT"
+              for core in $CORES; do
+                  echo powersave > /sys/devices/system/cpu/cpu$core/cpufreq/scaling_governor
+              done
+          fi
+      fi
+    '';
+    mode = "0755";
+  };
   boot.initrd = {
     availableKernelModules = [
       "virtio_net"
