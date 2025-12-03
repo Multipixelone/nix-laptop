3fc813f9438e4a29b55fa242adf86a4b1cbc18c3
Multipixelone <finn@cnwr.net>
2025-12-02 23:25:44 -0500

    add win script & desktop icon

--- home/zelda.nix
+++ home/zelda.nix
@@ -13,4 +13,31 @@
       win.quickSplash = "yes";
     };
   };
+  home.packages = [
+    (pkgs.writeScriptBin "win" ''
+      #!${lib.getExe pkgs.fish}
+      #!/usr/bin/env fish
+      set VM_NAME "win11"
+      set STATE (virsh --connect qemu:///system domstate $VM_NAME 2>/dev/null)
+      if string match -q "running*" $STATE
+          echo "$VM_NAME is already running."
+      else
+          echo "Starting $VM_NAME..."
+          virsh --connect qemu:///system start $VM_NAME
+          sleep 3
+      end
+
+      set -e WAYLAND_DISPLAY
+      looking-glass-client &
+      exit
+    '')
+    (pkgs.makeDesktopItem {
+      name = "windows";
+      desktopName = "Windows VM";
+      exec = "win";
+      terminal = false;
+      type = "Application";
+      icon = "windows95";
+    })
+  ];
 }
