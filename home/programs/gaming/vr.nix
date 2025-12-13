1293dfe0a427d85b811f6cec6dcc5a0374e01390
Multipixelone <finn@cnwr.net>
2025-12-12 23:53:25 -0500

    add wlr overlay

--- home/programs/gaming/vr.nix
+++ home/programs/gaming/vr.nix
@@ -1,8 +1,72 @@
 {
+  lib,
   pkgs,
+  osConfig,
   ...
 }:
+let
+  inherit (lib) getExe;
+in
 {
   xdg.configFile."openxr/1/active_runtime.json".source =
     "${pkgs.monado}/share/openxr/1/openxr_monado.json";
+  home.packages = [ pkgs.wlx-overlay-s ];
+
+  systemd.user.services.wlx-overlay-s-openxr = {
+    Unit = {
+      Description = "OpenXR Overlay";
+      After = [
+        "graphical-session.target"
+        "monado.service"
+      ];
+
+      PartOf = [
+        "monado.service"
+        "graphical-session.target"
+      ];
+
+      Requisite = [
+        "graphical-session.target"
+        "monado.service"
+        "monado.socket"
+      ];
+    };
+
+    Service = {
+      ExecStart = "${getExe pkgs.wlx-overlay-s} --show";
+    };
+
+    Install.WantedBy = [ "monado.service" ];
+  };
+  systemd.user.services.wlx-overlay-s-openvr = {
+    Unit = {
+      Description = "OpenVR Overlay";
+      After = [ "graphical-session.target" ];
+      PartOf = [ "graphical-session.target" ];
+      Requisite = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      ExecStart = "${getExe pkgs.wlx-overlay-s} --openvr --show";
+    };
+  };
+  # xdg.configFile."openvr/openvrpaths.vrpath".text =
+  #   let
+  #     steam = "${config.xdg.dataHome}/Steam";
+  #   in
+  #   builtins.toJSON {
+  #     version = 1;
+  #     jsonid = "vrpathreg";
+
+  #     external_drivers = null;
+  #     config = [ "${steam}/config" ];
+
+  #     log = [ "${steam}/logs" ];
+
+  #     "runtime" = [
+  #       "${pkgs.xrizer}/lib/xrizer"
+  #       # OR
+  #       #"${pkgs.opencomposite}/lib/opencomposite"
+  #     ];
+  #   };
 }
