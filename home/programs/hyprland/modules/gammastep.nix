0a2958d2745c7051d7fcf7d52c82ad9288d3e5f6
Multipixelone <finn@cnwr.net>
2025-12-18 18:59:23 -0500

    gammastep: switch to geoclue2

--- home/programs/hyprland/modules/gammastep.nix
+++ home/programs/hyprland/modules/gammastep.nix
@@ -6,20 +6,17 @@ in
   services.gammastep = {
     enable = true;
     tray = true;
+    provider = "geoclue2";
     temperature = {
       day = 6500;
       night = 4000;
     };
-    # manhatten ny
-    latitude = "40.6";
-    longitude = "-73.9";
     # https://gitlab.com/chinstrap/gammastep/-/blob/master/gammastep.conf.sample?ref_type=heads
     settings = {
       general = {
         fade = "1";
         brightness-day = "1.0";
         brightness-night = "0.8";
-        location-provider = "manual";
         gamma-day = "1.0";
         gamma-night = "0.8";
       };
