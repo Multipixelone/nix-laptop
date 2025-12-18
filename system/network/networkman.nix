855f9875ded68d7246ae5e500d8cc87b35b67c70
Multipixelone <finn@cnwr.net>
2025-12-18 18:57:37 -0500

    specify geoclue url & allow gammastep

--- system/network/networkman.nix
+++ system/network/networkman.nix
@@ -5,6 +5,16 @@
     dconf.enable = true;
   };
   services = {
-    geoclue2.enable = true;
+    geoclue2 = {
+      enable = true;
+      geoProviderUrl = "https://beacondb.net/v1/geolocate";
+      submissionUrl = "https://beacondb.net/v2/geosubmit";
+      submissionNick = "geoclue";
+
+      appConfig.gammastep = {
+        isAllowed = true;
+        isSystem = false;
+      };
+    };
   };
 }
