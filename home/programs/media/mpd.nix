241b78d30a5d41ee8ee2a6c5052ca7dda53720f8
Multipixelone <finn@cnwr.net>
2025-12-22 03:28:08 -0500

    mpd: output audio to fifo for cava

--- home/programs/media/mpd.nix
+++ home/programs/media/mpd.nix
@@ -13,6 +13,12 @@
       musicDirectory = config.home.sessionVariables.MUSIC_DIR;
       extraConfig = ''
         audio_output {
+           type   "fifo"
+           name   "my_fifo"
+           path   "/tmp/mpd.fifo"
+           format "44100:16:2"
+        }
+        audio_output {
           type "pipewire"
           name "PipeWire Output"
         }
