420d0020cefc8f500db6e7b6c8b12b9fbbcdf6d1
Multipixelone <finn@cnwr.net>
2025-12-26 13:45:35 -0800

    properly quote previous args

--- home/programs/terminal/wrapped.nix
+++ home/programs/terminal/wrapped.nix
@@ -9,26 +9,26 @@ let
   # wrap secret into gh cli
   gh-wrapped = pkgs.writeShellScriptBin "gh" ''
     export GITHUB_TOKEN=$(cat ${config.age.secrets."gh".path})
-    ${lib.getExe pkgs.gh} $@
+    ${lib.getExe pkgs.gh} "$@"
   '';
   # wrap secret into tgpt
   tgpt-wrapped = pkgs.writeShellScriptBin "tgpt" ''
     export AI_PROVIDER="openai"
     export OPENAI_MODEL="gpt-4o-mini" # use 4o-mini TODO: use bash variable expansion to set this value if unset.
     export OPENAI_API_KEY=$(cat ${config.age.secrets."openai".path})
-    ${lib.getExe pkgs.tgpt} $@
+    ${lib.getExe pkgs.tgpt} "$@"
   '';
   # wrap secret into todoist
   todoist-wrapped = pkgs.writeShellScriptBin "td" ''
     export TODOIST_TOKEN=$(cat ${config.age.secrets."todoist".path})
-    ${lib.getExe pkgs.todoist} $@
+    ${lib.getExe pkgs.todoist} "$@"
   '';
   # wrap client id and secret into gcalcli
   gcalcli-wrapped = pkgs.writeShellScriptBin "gcalcli" ''
     ${lib.getExe pkgs.gcalcli} \
     --client-id=$(cat ${config.age.secrets."gcalclient".path}) \
     --client-secret=$(cat ${config.age.secrets."gcalsecret".path}) \
-    $@
+    "$@"
   '';
 in
 {
