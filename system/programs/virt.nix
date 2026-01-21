32bd79b487ad446d575f4cb6d21bd110143ce9f1
Multipixelone <finn@cnwr.net>
2026-01-20 23:33:28 -0500

    virt: install virtualbox

--- system/programs/virt.nix
+++ system/programs/virt.nix
@@ -45,8 +45,13 @@
       "virtio_rng"
     ];
   };
+  users.extraGroups.vboxusers.members = [ "tunnel" ];
   virtualisation = {
     # oci-containers.backend = "docker";
+    virtualbox.host = {
+      enable = true;
+      enableExtensionPack = true;
+    };
     libvirtd = {
       enable = true;
       onBoot = "ignore";
