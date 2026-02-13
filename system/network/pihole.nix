07695a27307fbed232f8c86943a1e0ac36152bbc
Multipixelone <finn@cnwr.net>
2026-02-13 11:29:33 -0500

    specify specific localhost & wireguard addresses

--- system/network/pihole.nix
+++ system/network/pihole.nix
@@ -2,12 +2,15 @@
 {
   # Disable systemd-resolved to allow blocky to bind to port 53
   services.resolved.enable = false;
-  
+
   services.blocky = {
     enable = true;
     settings = {
       # Port configuration - listen on 53 for client queries
-      ports.dns = 53;
+      ports.dns = [
+        "127.0.0.1:53"
+        "10.100.0.1:53"
+      ];
 
       # Upstream DNS servers - forward to unbound
       upstreams = {
