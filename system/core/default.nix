b8e0a187a81e6f0742cc9a52e876f7798e959958
Multipixelone <finn@cnwr.net>
2026-02-15 10:45:19 -0500

    fix: increase open file limit

--- system/core/default.nix
+++ system/core/default.nix
@@ -22,6 +22,22 @@
     ];
   };
 
+  # Increase open file limit for sudoers
+  security.pam.loginLimits = [
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "soft";
+      value = "524288";
+    }
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "hard";
+      value = "1048576";
+    }
+  ];
+
   # don't touch this
   system.stateVersion = lib.mkDefault "23.11";
 
