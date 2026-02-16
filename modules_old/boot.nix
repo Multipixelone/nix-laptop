76b5c7da6ba394fa3a92e7635f9469ccbe36e1ef
Multipixelone <finn@cnwr.net>
2026-02-16 01:52:58 -0500

    move boot module

--- modules_old/boot.nix
+++ modules_old/boot.nix
@@ -4,34 +4,4 @@
   ...
 }:
 {
-  boot = {
-    loader.limine = {
-      enable = lib.mkDefault true;
-      panicOnChecksumMismatch = true;
-      style.wallpapers = [ ];
-      additionalFiles = {
-        "efi/memtest86+/memtest.efi" = "${pkgs.memtest86plus}/mt86plus.efi";
-      };
-      extraEntries = ''
-        /Memtest86+
-        	protocol: chainload
-        	path: boot():///efi/memtest86+/memtest.efi
-      '';
-    };
-    loader.grub = {
-      enable = lib.mkDefault false;
-      efiSupport = true;
-      device = "nodev";
-      # gfxmodeEfi = "3440x1440";
-      # lib.mkForce font = true;
-      # timeoutStyle = "hidden";
-    };
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    # hide os choice
-    loader.timeout = 3;
-    # xanmod or zen
-    # kernelPackages = pkgs.linuxPackages_xanmod_latest;
-    kernelPackages = lib.mkDefault pkgs.linuxPackages_xanmod_latest;
-  };
 }
