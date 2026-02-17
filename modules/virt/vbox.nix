77f7d2255b45b06bfca0825dce805a87b235670b
Multipixelone <finn@cnwr.net>
2026-02-16 22:45:30 -0500

    properly define config in attr set

--- modules/virt/vbox.nix
+++ modules/virt/vbox.nix
@@ -1,14 +1,13 @@
+{ config, ... }:
 {
   nixpkgs.config.allowUnfreePackages = [ "Oracle_VirtualBox_Extension_Pack" ];
-  flake.modules.nixos.pc =
-    { config, ... }:
-    {
-      users.extraGroups.vboxusers.members = [ config.flake.meta.owner.username ];
-      virtualisation = {
-        virtualbox.host = {
-          enable = true;
-          enableExtensionPack = true;
-        };
+  flake.modules.nixos.pc = {
+    users.extraGroups.vboxusers.members = [ config.flake.meta.owner.username ];
+    virtualisation = {
+      virtualbox.host = {
+        enable = true;
+        enableExtensionPack = true;
       };
     };
+  };
 }
