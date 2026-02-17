cb1e44093d282225feee7fce67e0bba1114b2454
Multipixelone <finn@cnwr.net>
2026-02-16 22:41:18 -0500

    proper config.flake use

--- modules/virt/vbox.nix
+++ modules/virt/vbox.nix
@@ -1,9 +1,9 @@
 {
   nixpkgs.config.allowUnfreePackages = [ "Oracle_VirtualBox_Extension_Pack" ];
   flake.modules.nixos.pc =
-    { flake, ... }:
+    { config, ... }:
     {
-      users.extraGroups.vboxusers.members = [ flake.meta.owner.username ];
+      users.extraGroups.vboxusers.members = [ config.flake.meta.owner.username ];
       virtualisation = {
         virtualbox.host = {
           enable = true;
