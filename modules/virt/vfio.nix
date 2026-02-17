4134c210b9dd5f4463f7afce13e4a596e5bda2bb
Multipixelone <finn@cnwr.net>
2026-02-17 12:24:05 -0500

    move looking-glass stuff to vfio

--- modules/virt/vfio.nix
+++ modules/virt/vfio.nix
@@ -3,6 +3,36 @@
   flake.modules.nixos.laptop =
     { pkgs, ... }:
     {
+      boot.extraModulePackages = with config.boot.kernelPackages; [ kvmfr ];
+      boot.extraModprobeConfig = ''
+        options kvmfr static_size_mb=64
+      '';
+      boot.kernelModules = [ "kvmfr" ];
+
+      services.udev.extraRules = ''
+        SUBSYSTEM=="kvmfr", GROUP="kvm", MODE="0660", TAG+="systemd"
+      '';
+      boot.kernel.sysctl = {
+        "vm.compaction_proactiveness" = 0;
+      };
+      boot.kernelParams = [
+        # cpu isol
+        "isolcpus=0-15"
+        "nohz_full=0-15"
+        "rcu_nocbs=0-15"
+        # 20GB = 20 * 1024 / 2 = 10240 2MB pages (add buffer:  10500)
+        "hugepagesz=2M"
+        "hugepages=10500"
+        "transparent_hugepage=never"
+      ];
+      systemd.mounts = [
+        {
+          what = "hugetlbfs";
+          where = "/dev/hugepages";
+          type = "hugetlbfs";
+          options = "mode=0775,gid=libvirtd";
+        }
+      ];
       environment.etc."libvirt/hooks/qemu" = {
         source = pkgs.writeText "qemu" ''
           #!/usr/bin/env bash
