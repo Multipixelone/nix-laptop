6f3ac0741c481a4a8f01eb8d3b171828f056112c
Multipixelone <finn@cnwr.net>
2026-02-17 20:32:46 -0500

    add pager

---
+++ modules/shell/pager.nix
@@ -0,0 +1,27 @@
+{
+  flake.modules.homeManager.base =
+    {
+      pkgs,
+      lib,
+      ...
+    }:
+    let
+      ovCommand = lib.getExe pkgs.ov;
+    in
+    {
+      programs = {
+        fish.functions.__fish_anypager = "echo ${ovCommand}";
+      };
+      home = {
+        sessionVariables = {
+          PAGER = ovCommand;
+          MANPAGER = "${ovCommand} --section-delimiter '^[^\\s]' --section-header";
+          BAT_PAGER = "${ovCommand} --quit-if-one-screen --header 3";
+        };
+        packages = with pkgs; [
+          delta
+          ov
+        ];
+      };
+    };
+}
