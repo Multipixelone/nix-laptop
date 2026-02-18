cc199376d99e4249a5065284b5ab1ab53e5009de
Multipixelone <finn@cnwr.net>
2026-02-17 19:07:30 -0500

    bat & btop

---
+++ modules/shell/bat.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  ...
+}:
+{
+  flake.modules.homeManager.base =
+    {
+      pkgs,
+      lib,
+      ...
+    }:
+    let
+      # unpaged bat instead of cat
+      bat-wrapped = pkgs.writeShellApplication {
+        name = "cat";
+        runtimeInputs = [
+          config.programs.bat.package
+        ];
+        text = ''
+          bat --style=header -P "$@"
+        '';
+      };
+    in
+    {
+      programs = {
+        fish.shellAliases.cat = lib.getExe bat-wrapped;
+        bat = {
+          enable = true;
+          config = {
+            pager = "${lib.getExe pkgs.ov} --quit-if-one-screen --header 3";
+          };
+        };
+      };
+    };
+}
