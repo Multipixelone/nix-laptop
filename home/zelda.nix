b17afac080fb9cd1791a6b9baf0e035a6b8dacc1
Multipixelone <finn@cnwr.net>
2026-02-18 01:32:38 -0500

    move stuff to vfio module

--- home/zelda.nix
+++ home/zelda.nix
@@ -63,47 +63,7 @@ in
       };
     };
   };
-  programs.looking-glass-client = {
-    enable = true;
-    settings = {
-      app.shmFile = "/dev/kvmfr0";
-      app.allowDMA = "yes";
-      input.escapeKey = 119;
-      input.rawMouse = "yes";
-      spice.enable = "yes";
-      win.autoScreensaver = "yes";
-      win.fullScreen = "yes";
-      win.jitRender = "yes";
-      win.quickSplash = "yes";
-      wayland.fractionScale = "no";
-    };
-  };
   home.packages = [
     sunpaper
-    (pkgs.writeScriptBin "win" ''
-      #!${lib.getExe pkgs.fish}
-      #!/usr/bin/env fish
-      set VM_NAME "win11"
-      set STATE (virsh --connect qemu:///system domstate $VM_NAME 2>/dev/null)
-      if string match -q "running*" $STATE
-          echo "$VM_NAME is already running."
-      else
-          echo "Starting $VM_NAME..."
-          virsh --connect qemu:///system start $VM_NAME
-          sleep 3
-      end
-
-      set -e WAYLAND_DISPLAY
-      looking-glass-client &
-      exit
-    '')
-    (pkgs.makeDesktopItem {
-      name = "windows";
-      desktopName = "Windows VM";
-      exec = "win";
-      terminal = false;
-      type = "Application";
-      icon = "windows95";
-    })
   ];
 }
