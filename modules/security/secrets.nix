94f0f895520a103218406dc52348357a0f63e847
Multipixelone <finn@cnwr.net>
2026-02-18 01:32:31 -0500

    fix secrets module

--- modules/security/secrets.nix
+++ modules/security/secrets.nix
@@ -1,45 +1,46 @@
 {
   inputs,
-  lib,
   ...
 }:
 let
   polyModule = {
-    agenix = {
-      enable = true;
-      enableReleaseChecks = false;
-    };
   };
 in
 {
   flake.modules = {
     nixos.base = {
       imports = [
-        inputs.agenix.nixosModules.agenix
+        inputs.agenix.nixosModules.default
         polyModule
       ];
-      agenix.homeManagerIntegration.autoImport = false;
+      # agenix.homeManagerIntegration.autoImport = false;
     };
 
     homeManager.base = {
       imports = [
-        inputs.agenix.homeModules.agenix
+        inputs.agenix.homeManagerModules.default
         polyModule
       ];
+      age = {
+        identityPaths = [
+          "/home/tunnel/.ssh/agenix"
+        ];
+        secretsDir = "/home/tunnel/.secrets";
+      };
     };
 
-    nixOnDroid.base = {
-      imports = [
-        inputs.agenix.nixOnDroidModules.agenix
-        polyModule
-      ];
-    };
+    # nixOnDroid.base = {
+    #   imports = [
+    #     inputs.agenix.nixOnDroidModules.agenix
+    #     polyModule
+    #   ];
+    # };
 
-    nixvim.base = nixvimArgs: {
-      # https://github.com/danth/agenix/pull/415#issuecomment-2832398958
-      imports = lib.optional (
-        nixvimArgs ? homeConfig
-      ) nixvimArgs.homeConfig.agenix.targets.nixvim.exportedModule;
-    };
+    # nixvim.base = nixvimArgs: {
+    #   # https://github.com/danth/agenix/pull/415#issuecomment-2832398958
+    #   imports = lib.optional (
+    #     nixvimArgs ? homeConfig
+    #   ) nixvimArgs.homeConfig.agenix.targets.nixvim.exportedModule;
+    # };
   };
 }
