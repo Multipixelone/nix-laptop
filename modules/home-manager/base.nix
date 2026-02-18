519fe62001040eccb793969e7716fa91ab985cfd
Multipixelone <finn@cnwr.net>
2026-02-17 22:31:10 -0500

    linger & install all outputs

--- modules/home-manager/base.nix
+++ modules/home-manager/base.nix
@@ -4,6 +4,13 @@
     home = {
       username = config.flake.meta.owner.username;
       homeDirectory = "/home/${config.flake.meta.owner.username}";
+      # required options for quadlet-nix
+      linger = true; # start pods before user logs in
+      autoSubUidGidRange = true;
+      extraOutputsToInstall = [
+        "doc"
+        "devdoc"
+      ];
     };
     programs.home-manager.enable = true;
     systemd.user.startServices = "sd-switch";
