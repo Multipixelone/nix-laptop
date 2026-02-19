56ebb7aa7f3e4614788d1fb54cf3b3da684151e1
Multipixelone <finn@cnwr.net>
2026-02-19 01:10:23 -0500

    iso: enable graphics

--- modules/iso/imports.nix
+++ modules/iso/imports.nix
@@ -1,6 +1,7 @@
 { inputs, config, ... }:
 {
   configurations.nixos.iso.module = {
+    hardware.graphics.enable = true;
     imports = with config.flake.modules.nixos; [
       "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix"
       "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
