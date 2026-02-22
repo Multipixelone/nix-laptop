cf4ac19b664cf66f5e1838218334fc2d1a834ff0
Multipixelone <finn@cnwr.net>
2026-02-22 12:54:17 -0500

    performance governor

--- modules/link/cpu.nix
+++ modules/link/cpu.nix
@@ -1,5 +1,8 @@
 {
   configurations.nixos.link.module = {
+    # hint epp to use maximum cpu performance
+    powerManagement.cpuFreqGovernor = "performance";
+
     services.ucodenix = {
       enable = true;
       cpuModelId = "00A20F10";
