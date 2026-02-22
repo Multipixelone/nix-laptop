c348d0bb572f7955ded1066c1b7378d583ff5fbb
Multipixelone <finn@cnwr.net>
2026-02-22 13:46:58 -0500

    only autoscrub if btrfs filesystems mounted

--- modules/hardware/btrfs.nix
+++ modules/hardware/btrfs.nix
@@ -1,10 +1,14 @@
 {
   flake.modules = {
-    nixos.base = {
-      services = {
-        fstrim.enable = true;
-        btrfs.autoScrub.enable = true;
+    nixos.base =
+      { config, lib, ... }:
+      {
+        services = {
+          fstrim.enable = true;
+          btrfs.autoScrub.enable = lib.mkDefault (
+            lib.any (fs: fs.fsType == "btrfs") (lib.attrValues config.fileSystems)
+          );
+        };
       };
-    };
   };
 }
