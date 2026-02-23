9326d9c7de708ecb71c80716215245d406b6cbb1
Multipixelone <finn@cnwr.net>
2026-02-22 19:47:56 -0500

    shell = fish

--- modules/owner.nix
+++ modules/owner.nix
@@ -8,18 +8,21 @@
     };
 
     modules = {
-      nixos.base = {
-        users.users.${config.flake.meta.owner.username} = {
-          isNormalUser = true;
-          initialPassword = "";
-          # required options for quadlet-nix
-          linger = true; # start pods before user logs in
-          autoSubUidGidRange = true;
-          extraGroups = [ "input" ];
-        };
+      nixos.base =
+        { pkgs, ... }:
+        {
+          users.users.${config.flake.meta.owner.username} = {
+            isNormalUser = true;
+            initialPassword = "";
+            shell = pkgs.fish;
+            # required options for quadlet-nix
+            linger = true; # start pods before user logs in
+            autoSubUidGidRange = true;
+            extraGroups = [ "input" ];
+          };
 
-        nix.settings.trusted-users = [ config.flake.meta.owner.username ];
-      };
+          nix.settings.trusted-users = [ config.flake.meta.owner.username ];
+        };
     };
   };
 }
