539caf3fb962ec133402dfbbcb8ee70c48d0a7fa
Multipixelone <finn@cnwr.net>
2026-02-22 19:47:46 -0500

    add user to i2c

--- modules/hardware/rgb.nix
+++ modules/hardware/rgb.nix
@@ -1,5 +1,7 @@
+{ config, ... }:
 {
   flake.modules.nixos.gaming = {
+    users.extraGroups.i2c.members = [ config.flake.meta.owner.username ];
     programs.coolercontrol.enable = true;
     hardware.i2c.enable = true;
     boot.kernelParams = [ "acpi_enforce_resources=lax" ];
