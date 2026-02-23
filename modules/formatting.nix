eeab762f66550e4829aadc666a5250547b744888
Multipixelone <finn@cnwr.net>
2026-02-22 22:37:08 -0500

    add treefmt-nix system

---
+++ modules/formatting.nix
@@ -0,0 +1,23 @@
+{ inputs, ... }:
+{
+  imports = [ inputs.treefmt-nix.flakeModule ];
+
+  perSystem = {
+    treefmt = {
+      projectRootFile = "flake.nix";
+      programs = {
+        prettier.enable = true;
+        shfmt.enable = true;
+      };
+      settings = {
+        on-unmatched = "fatal";
+        global.excludes = [
+          "*.jpg"
+          "*.png"
+          "LICENSE"
+        ];
+      };
+    };
+    pre-commit.settings.hooks.treefmt.enable = true;
+  };
+}
