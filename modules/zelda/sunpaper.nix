e08b41e0bc79e8aa47d7700631dca9fae5b10b14
Multipixelone <finn@cnwr.net>
2026-02-24 20:14:38 -0500

    proper passing of home manager config args

--- modules/zelda/sunpaper.nix
+++ modules/zelda/sunpaper.nix
@@ -1,11 +1,7 @@
 { lib, ... }:
 {
   configurations.nixos.zelda.module =
-    {
-      pkgs,
-      config,
-      ...
-    }:
+    { pkgs, ... }:
     let
       sunpaper = pkgs.sunpaper.overrideAttrs (oldAttrs: {
         postPatch = ''
@@ -22,50 +18,52 @@
       });
     in
     {
-      home-manager.users.tunnel = {
-        home.sessionVariables = lib.mkForce {
-          MUSIC_DIR = "${config.xdg.userDirs.music}/Library";
-          TRANSCODED_MUSIC = "${config.xdg.userDirs.music}/Library";
-          ARTWORK_DIR = "${config.xdg.userDirs.music}/RockboxCover";
-          MOPIDY_PLAYLISTS = "/home/tunnel/.local/share/mopidy/m3u";
-          IPOD_DIR = "/run/media/tunnel/FINNR_S IPO";
-          PLAYLIST_DIR = "/home/tunnel/Music/Playlists";
-        };
-        xdg.configFile."sunpaper/config".text = ''
-          latitude="40.680271N"
-          longitude="73.944893W"
+      home-manager.users.tunnel =
+        { config, ... }:
+        {
+          home.sessionVariables = lib.mkForce {
+            MUSIC_DIR = "${config.xdg.userDirs.music}/Library";
+            TRANSCODED_MUSIC = "${config.xdg.userDirs.music}/Library";
+            ARTWORK_DIR = "${config.xdg.userDirs.music}/RockboxCover";
+            MOPIDY_PLAYLISTS = "/home/tunnel/.local/share/mopidy/m3u";
+            IPOD_DIR = "/run/media/tunnel/FINNR_S IPO";
+            PLAYLIST_DIR = "/home/tunnel/Music/Playlists";
+          };
+          xdg.configFile."sunpaper/config".text = ''
+            latitude="40.680271N"
+            longitude="73.944893W"
 
-          swww_enable="true"
-          swww_fps="240"
-          swww_step="30"
-        '';
-        systemd.user = {
-          timers.sunpaper = {
-            Install.WantedBy = [ "timers.target" ];
-            Timer = {
-              OnCalendar = "*:0/1";
+            swww_enable="true"
+            swww_fps="240"
+            swww_step="30"
+          '';
+          systemd.user = {
+            timers.sunpaper = {
+              Install.WantedBy = [ "timers.target" ];
+              Timer = {
+                OnCalendar = "*:0/1";
+              };
+            };
+            services.sunpaper = {
+              Unit.Description = "automatic wallpaper set based on sun";
+              Install.WantedBy = [ "graphical-session.target" ];
+              Service = {
+                ExecStart = lib.getExe sunpaper;
+              };
+            };
+            services.sunpaper-cache = {
+              Unit.Description = "clear sunpaper cache on startup";
+              Install.WantedBy = [ "graphical-session.target" ];
+              Service = {
+                Type = "oneshot";
+                RemainAfterExit = "no";
+                ExecStart = "${lib.getExe sunpaper} -c";
+              };
             };
           };
-          services.sunpaper = {
-            Unit.Description = "automatic wallpaper set based on sun";
-            Install.WantedBy = [ "graphical-session.target" ];
-            Service = {
-              ExecStart = lib.getExe sunpaper;
-            };
-          };
-          services.sunpaper-cache = {
-            Unit.Description = "clear sunpaper cache on startup";
-            Install.WantedBy = [ "graphical-session.target" ];
-            Service = {
-              Type = "oneshot";
-              RemainAfterExit = "no";
-              ExecStart = "${lib.getExe sunpaper} -c";
-            };
-          };
+          home.packages = [
+            sunpaper
+          ];
         };
-        home.packages = [
-          sunpaper
-        ];
-      };
     };
 }
