427307f8af82b1dec6495bd3fa63a06a02ce71e5
Multipixelone <finn@cnwr.net>
2026-02-25 01:38:50 -0500

    define public key for hosts

---
+++ modules/link/ssh.nix
@@ -0,0 +1,5 @@
+{
+  configurations.nixos.link.module = {
+    services.openssh.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfFq+1W21NoXAyFc1HT5zJ7GAVDbQw/f6reJI3X2vtn link";
+  };
+}
---
+++ modules/zelda/ssh.nix
@@ -0,0 +1,5 @@
+{
+  configurations.nixos.link.module = {
+    services.openssh.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK5bqb1RiYN2X5dx4GKlTgeiWhYWHQhiV/HU1MtOZfFt zelda";
+  };
+}
