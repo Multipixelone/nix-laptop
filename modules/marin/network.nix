c5ec33511f203629a17d3df2975628c751807f9a
Multipixelone <finn@cnwr.net>
2026-02-26 08:59:13 -0500

    feat: networkmanager profiles on marin

--- modules/marin/network.nix
+++ modules/marin/network.nix
@@ -1,38 +1,70 @@
 {
-  pkgs,
+  inputs,
+  config,
   ...
 }:
+let
+  marinHost = config.hosts.marin;
+in
 {
+  configurations.nixos.marin.module =
+    { config, ... }:
+    {
+      age.secrets."wifi".file = "${inputs.secrets}/wifi/home.age";
 
-  configurations.nixos.marin.module = {
-    # nh default flake
-    environment.variables.NH_FLAKE = "/home/tunnel/nix-laptop";
-
-    networking = {
-      nameservers = [
-        "8.8.8.8"
-        "1.1.1.1"
-      ];
-      interfaces.enp3s0f0.ipv4.addresses = [
-        {
-          address = "192.168.7.3";
-          prefixLength = 24;
-        }
-      ];
-      interfaces.wlp2s0.ipv4.addresses = [
-        {
-          address = "192.168.5.21";
-          prefixLength = 24;
-        }
-      ];
-      # defaultGateway = {
-      #   address = "192.168.7.1";
-      #   interface = "enp3s0f0";
-      # };
-      defaultGateway = {
-        address = "192.168.5.1";
-        interface = "wlp2s0";
+      networking = {
+        nameservers = [
+          "8.8.8.8"
+          "1.1.1.1"
+        ];
+        networkmanager = {
+          enable = true;
+          ensureProfiles = {
+            environmentFiles = [ config.age.secrets."wifi".path ];
+            profiles = {
+              cjnfrw-iot = {
+                connection = {
+                  id = "cjnfrw-iot";
+                  type = "wifi";
+                  interface-name = "wlp2s0";
+                  autoconnect = true;
+                };
+                wifi = {
+                  mode = "infrastructure";
+                  ssid = "cjnfrw-iot";
+                };
+                wifi-security = {
+                  key-mgmt = "wpa-psk";
+                  psk = "$CJNFRW_IOT_PSK";
+                };
+                ipv4 = {
+                  address1 = "${marinHost.iotAddress}/24";
+                  gateway = "192.168.5.1";
+                  method = "manual";
+                };
+                ipv6 = {
+                  addr-gen-mode = "stable-privacy";
+                  method = "auto";
+                };
+              };
+              ethernet = {
+                connection = {
+                  id = "ethernet";
+                  type = "ethernet";
+                };
+                ipv4 = {
+                  address1 = "${marinHost.homeAddress}/24";
+                  gateway = "192.168.7.1";
+                  method = "manual";
+                };
+                ipv6 = {
+                  addr-gen-mode = "stable-privacy";
+                  method = "auto";
+                };
+              };
+            };
+          };
+        };
       };
     };
-  };
 }
