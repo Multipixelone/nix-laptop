797e0fdede555669c1b21e2b2a7a6a4fa177f078
Multipixelone <finn@cnwr.net>
2026-02-25 23:22:09 -0500

    create colmena alias blocks

--- modules/configurations/colmena.nix
+++ modules/configurations/colmena.nix
@@ -2,30 +2,33 @@
   lib,
   config,
   inputs,
+  withSystem,
   ...
 }:
 let
   deployableHosts = lib.filterAttrs (_: cfg: cfg.deployment != null) config.configurations.nixos;
   nixosConfigs = config.flake.nixosConfigurations;
-  hosts = config.hosts;
+  inherit (config) hosts;
   defaultTargetHost =
     name:
     let
       nixos = nixosConfigs.${name} or null;
     in
     if nixos != null then nixos.config.networking.fqdn else name;
+  # Check if host has a resolvable address (wireguard or home)
+  hasResolvableAddress =
+    name:
+    let
+      host = hosts.${name} or null;
+    in
+    host != null && (host.wireguard.ipv4Address != null || host.homeAddress != null);
+
   resolveTargetHost =
     name: cfg:
     if cfg.deployment.targetHost != null then
       cfg.deployment.targetHost
-    else if cfg.deployment.useWireguardAddress then
-      let
-        addr = (hosts.${name} or (throw "Host '${name}' has useWireguardAddress enabled but is not in the hosts registry")).wireguard.ipv4Address;
-      in
-      if addr != null then
-        addr
-      else
-        throw "Host '${name}' has useWireguardAddress enabled but no WireGuard address in the hosts registry"
+    else if hasResolvableAddress name then
+      "colmena.${name}"
     else
       (defaultTargetHost name);
 in
@@ -41,6 +44,12 @@ in
       deployment = {
         targetHost = resolveTargetHost name cfg;
         inherit (cfg.deployment) targetUser tags allowLocalDeployment;
+      };
+      nixpkgs = {
+        pkgs = lib.mkForce (
+          withSystem nixosConfigs.${name}.config.nixpkgs.hostPlatform.system ({ pkgs, ... }: pkgs)
+        );
+        config = lib.mkForce { };
       };
       imports = [ cfg.module ];
     }) deployableHosts
--- modules/ssh.nix
+++ modules/ssh.nix
@@ -10,6 +10,25 @@ let
         nixos.config.services.openssh.publicKey
       ])
     );
+
+  # Resolve host address: wireguard -> homeAddress -> null
+  resolveHostAddress =
+    name:
+    let
+      host = config.hosts.${name} or null;
+    in
+    if host == null then
+      null
+    else if host.wireguard.ipv4Address != null then
+      host.wireguard.ipv4Address
+    else if host.homeAddress != null then
+      host.homeAddress
+    else
+      null;
+
+  # Get deployable hosts with resolvable addresses
+  deployableHosts = lib.filterAttrs (_: cfg: cfg.deployment != null) config.configurations.nixos;
+  colmenaHosts = lib.filterAttrs (name: _: resolveHostAddress name != null) deployableHosts;
 in
 {
   flake.modules = {
@@ -47,6 +66,10 @@ in
           "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBGm2epY8mE3z7qoL10fXmBuv4EPHnQoqJoYrL9TgfJwhnZMsaf1FQ2jalGSCE6T+QuYF/WM+bIWxZiYrT/XisM= ipad"
         ];
 
+        users.users.root.openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOVPE0NP1EtHnnzhBXZ4Cz6YAw/ZaEFUA8T6YvtnzGcK colmena-deploy"
+        ];
+
         programs.ssh.knownHosts =
           reachableNixoss
           |> lib.mapAttrs (
@@ -71,6 +94,19 @@ in
                 identityFile = "~/.ssh/keys/id_ed25519";
               };
             }
+          )
+          |> lib.concat (
+            colmenaHosts
+            |> lib.mapAttrsToList (
+              name: _: {
+                "colmena.${name}" = {
+                  hostname = resolveHostAddress name;
+                  user = "root";
+                  identityFile = "${args.config.home.homeDirectory}/.ssh/colmena";
+                  identitiesOnly = true;
+                };
+              }
+            )
           )
           |> lib.concat [
             {
