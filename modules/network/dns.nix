e9581b0b297e518c20a0e71f6f57a414b0f3d324
Multipixelone <finn@cnwr.net>
2026-02-28 14:06:49 -0500

    add dns sni

--- modules/network/dns.nix
+++ modules/network/dns.nix
@@ -4,19 +4,20 @@
     services.resolved = {
       enable = lib.mkDefault true;
       settings.Resolve = {
-        DNSOverTls = "opportunistic";
+        DNSSEC = "allow-downgrade";
+        DNSOverTLS = "opportunistic";
         DNS = [
           # https://developers.cloudflare.com/1.1.1.1
-          "1.1.1.1"
-          "1.0.0.1"
-          "2606:4700:4700::1111"
-          "2606:4700:4700::1001"
+          "1.1.1.1#cloudflare-dns.com"
+          "1.0.0.1#cloudflare-dns.com"
+          "2606:4700:4700::1111#cloudflare-dns.com"
+          "2606:4700:4700::1001#cloudflare-dns.com"
 
           # https://developers.google.com/speed/public-dns/docs/using
-          "8.8.8.8"
-          "8.8.4.4"
-          "2001:4860:4860::8888"
-          "2001:4860:4860::8844"
+          "8.8.8.8#dns.google"
+          "8.8.4.4#dns.google"
+          "2001:4860:4860::8888#dns.google"
+          "2001:4860:4860::8844#dns.google"
         ];
       };
     };
