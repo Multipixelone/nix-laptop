c5bd37764d1febee19d0d15ac84d93c4cdca1594
Multipixelone <finn@cnwr.net>
2026-03-01 00:13:22 -0500

    fix: steam override with TZ profile

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -1,9 +1,37 @@
 { inputs, ... }:
 {
-  nixpkgs.config.allowUnfreePackages = [
-    "steam"
-    "steam-unwrapped"
-  ];
+  nixpkgs.config = {
+    allowUnfreePackages = [
+      "steam"
+      "steam-unwrapped"
+    ];
+    packageOverrides = pkgs: {
+      steam = pkgs.steam.override {
+        extraProfile = ''
+          # Fixes timezones
+          unset TZ
+          # Allows Monado to be used
+          export PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES=1
+        '';
+        extraPkgs =
+          pkgs: with pkgs; [
+            libXcursor
+            libXi
+            libXinerama
+            libXScrnSaver
+            libpng
+            libpulseaudio
+            libvorbis
+            stdenv.cc.cc.lib
+            libkrb5
+            keyutils
+            # Steam VR
+            procps
+            usbutils
+          ];
+      };
+    };
+  };
   flake.modules.nixos.gaming =
     { pkgs, ... }:
     {
