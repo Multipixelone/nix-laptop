a18408f03da161c65f21b4351400e06212bdcb4a
Multipixelone <finn@cnwr.net>
2026-03-04 16:22:46 -0500

    feat: add deluge client

--- modules/link/jdownloader.nix
+++ modules/link/jdownloader.nix
@@ -1,6 +1,9 @@
 {
   configurations.nixos.link.module = {
-    systemd.tmpfiles.rules = [ "d /srv/jdownloader 0770 tunnel users -" ];
+    systemd.tmpfiles.rules = [
+      "d /srv/jdownloader 0770 tunnel users -"
+      "d /srv/deluge 0770 tunnel users -"
+    ];
 
     virtualisation.oci-containers.containers.jdownloader = {
       autoStart = false;
@@ -11,6 +14,22 @@
       volumes = [
         "/media/Data/ImportMusic/JDownloader/:/output"
         "/srv/jdownloader/:/config"
+      ];
+    };
+    virtualisation.oci-containers.containers.deluge = {
+      autoStart = false;
+      image = "linuxserver/deluge:latest";
+      ports = [
+        "8112:8112"
+        "6881:6881"
+        "6881:6881/udp"
+        "58846:58846"
+      ];
+      # user = "tunnel:users";
+      # TODO find some universal way to declare these paths like my music library so that I can use a variable
+      volumes = [
+        "/media/Data/ImportMusic/JDownloader/:/downloads"
+        "/srv/deluge/:/config"
       ];
     };
   };
