9b3d416cdec5091ac4dfe11b86655aea81c339e1
Multipixelone <finn@cnwr.net>
2026-03-11 13:58:51 -0400

    fix: add user to video group

--- modules/hardware/backlight.nix
+++ modules/hardware/backlight.nix
@@ -1,6 +1,8 @@
+{ config, ... }:
 {
   flake.modules.nixos.laptop = {
     hardware.brillo.enable = true;
     hardware.acpilight.enable = true;
+    users.extraGroups.video.members = [ config.flake.meta.owner.username ];
   };
 }
