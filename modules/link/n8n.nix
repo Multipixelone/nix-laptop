a8af2d280443af1e7239328ac5a2ca59898c8306
Multipixelone <finn@cnwr.net>
2026-03-12 15:40:48 -0400

    feat: n8n enable runners with secret

--- modules/link/n8n.nix
+++ modules/link/n8n.nix
@@ -1,12 +1,23 @@
+{ inputs, ... }:
 {
   nixpkgs.config.allowUnfreePackages = [
     "n8n"
+    "n8n-task-runner-launcher"
   ];
-  configurations.nixos.link.module = {
-    services.n8n = {
-      enable = true;
-      openFirewall = true;
-      environment.N8N_SECURE_COOKIE = false;
+  configurations.nixos.link.module =
+    { config, ... }:
+    {
+      age.secrets."n8n".file = "${inputs.secrets}/n8n.age";
+      services.n8n = {
+        enable = true;
+        openFirewall = true;
+        environment = {
+          N8N_SECURE_COOKIE = false;
+          N8N_RUNNERS_AUTH_TOKEN_FILE = config.age.secrets."n8n".path;
+        };
+        taskRunners = {
+          enable = true;
+        };
+      };
     };
-  };
 }
