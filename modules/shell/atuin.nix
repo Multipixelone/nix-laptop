bd9e06ea7111148c987ba37e037b182b920a68dc
Multipixelone <finn@cnwr.net>
2026-03-14 22:32:38 -0400

    feat: session-preload default atuin search

--- modules/shell/atuin.nix
+++ modules/shell/atuin.nix
@@ -21,7 +21,7 @@
 
         search_mode = "fuzzy";
         filter_mode = "global";
-        filter_mode_shell_up_key_binding = "session";
+        filter_mode_shell_up_key_binding = "session-preload";
 
         style = "compact";
         inline_height = 20;
